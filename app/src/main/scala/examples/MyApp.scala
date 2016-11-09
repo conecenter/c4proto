a9997df2fc27751babd1484602c69b8aa7c4de44
Sergei Kirjanov <rewlad@gmail.com>
2016-11-09 14:10:33 +0200

    traits compiles

--- app/src/main/scala/examples/MyApp.scala
+++ app/src/main/scala/examples/MyApp.scala
@@ -3,16 +3,48 @@ package examples
 @main
 object MyApp {
   println("HI+")
+
+  trait Lens[M,V] {
+    def get(model: M): V
+    def set(model: M, value: V): M
+  }
+
+  def lens[M,V](get: M⇒V): Lens[M,V]
+
+  change(model.name, "Leader")
+
 }
 
-@schema
-object MySchema {
-  @Id(0x0003)
-  trait ModelA {
-    @Id(0x0004) def propA: Option[Int]
-  }
-  @Id(0x0001) trait ModelB {
-    @Id(0x0005) def propB: Option[ModelA]
-    @Id(0x0006) def propB0: Option[BigDecimal] @scale(2)
+
+object BigDecimalImpl {
+  def apply(scale: Int, bytes: okio.ByteString): BigDecimal =
+    BigDecimal(new java.math.BigDecimal(new java.math.BigInteger(bytes.toByteArray), scale))
+  def unapply(value: BigDecimal): Option[(Int,okio.ByteString)] = {
+    val bytes = value.bigDecimal.unscaledValue.toByteArray
+    Option((value.bigDecimal.scale, okio.ByteString.of(bytes,0,bytes.length)))
   }
 }
+
+@schema object DecimalSchema {
+  trait SysBigDecimal {
+    @Id(0x0001) def scale: Int
+    @Id(0x0002) def bytes: okio.ByteString
+  }
+}
+
+
+
+
+@schema object MySchema {
+  import DecimalSchema._
+  @Id(0x0003)
+  trait ChildModel {
+    @Id(0x0007) def name: String
+    @Id(0x0004) def age: Option[BigDecimal] @scale(0)
+  }
+  @Id(0x0001) trait ParentModel {
+    @Id(0x0005) def leader: Option[ChildModel]
+    @Id(0x0006) def worker: List[ChildModel]
+  }
+}
+
--- macros/src/main/scala/examples/main.scala
+++ macros/src/main/scala/examples/main.scala
@@ -3,9 +3,6 @@ package examples
 
 import scala.annotation.{StaticAnnotation, compileTimeOnly}
 import scala.meta._
-import scala.collection.immutable
-import scala.meta.Ctor.Ref.Name
-import scala.meta.Defn.Def
 
 @compileTimeOnly("@examples.Main not expanded")
 class main extends scala.annotation.StaticAnnotation {
@@ -32,71 +29,83 @@ case class ProtoProp(
   initDecodeStatement: String,
   decodeCase: String,
   constructArg: String,
-  resultFix: String,
-  isSystem: Boolean
+  resultFix: String
 )
-
 case class ProtoType(
-    encodeStatement: String, serializerType: String, empty: String, resultType: String,
-    resultFix: String="", reduce: (String,String)=("",""), isSystem: Boolean=false
+    encodeStatement: (String,String), serializerType: String, empty: String, resultType: String,
+    resultFix: String="", reduce: (String,String)=("","")
 )
 case class ProtoMessage(adapterName: String, classImpl: String, adapterImpl: String)
+case class ProtoMods(id: Option[Int]=None)
 
 @compileTimeOnly("not expanded")
 class schema extends scala.annotation.StaticAnnotation {
   inline def apply(defn: Any): Any = meta {
     val q"object $objectName { ..$stats }" = defn
-    val messages: List[ProtoMessage] = stats.map{
+    val messages: List[ProtoMessage] = stats.flatMap{
+      case q"import ..$i" ⇒ None
       case q"..$mods trait ${Type.Name(messageName)} { ..$stats }" =>
-        def idOpt(mods: Seq[Mod]): Option[Int] = mods match {
-          case Seq() ⇒ None
-          case Seq(mod"@Id(${Lit(id:Int)})") ⇒ Option(id)
-        }
-        val id: Option[Int] = idOpt(mods)
+        val protoMods = mods./:(ProtoMods())((pMods,mod)⇒ mod match {
+          case mod"@Id(${Lit(id:Int)})" if pMods.id.isEmpty ⇒
+            pMods.copy(id=Option(id))
+        })
         val props: List[ProtoProp] = stats.map{
           case q"..$mods def ${Term.Name(propName)}: $tpe" ⇒
-            val id = idOpt(mods).get
+            val Seq(mod"@Id(${Lit(id:Int)})") = mods
             val pt: ProtoType = tpe match {
               case t"Int" ⇒
                 ProtoType(
-                  s"if(prep_$propName != 0){ val item = prep_$propName; ",
-                  "com.squareup.wire.ProtoAdapter.SINT32", "0", "Int",
-                  isSystem=true
+                  encodeStatement = (s"if(prep_$propName != 0)", s"prep_$propName)"),
+                  serializerType = "com.squareup.wire.ProtoAdapter.SINT32",
+                  empty = "0",
+                  resultType = "Int"
                 )
               case t"Long" ⇒
                 ProtoType(
-                  s"if(prep_$propName != 0L){ val item = prep_$propName; ",
-                  "com.squareup.wire.ProtoAdapter.SINT62", "0", "Long",
-                  isSystem=true
+                  encodeStatement = (s"if(prep_$propName != 0L)", s"prep_$propName)"),
+                  serializerType = "com.squareup.wire.ProtoAdapter.SINT62",
+                  empty = "0",
+                  resultType = "Long"
                 )
               case t"okio.ByteString" ⇒
                 ProtoType(
-                  s"if(prep_$propName.nonEmpty){ val item = prep_$propName; ",
-                  "com.squareup.wire.ProtoAdapter.BYTES", "okio.ByteString.EMPRY", "okio.ByteString",
-                  isSystem=true
+                  encodeStatement = (s"if(prep_$propName.size > 0)", s"prep_$propName)"),
+                  serializerType = "com.squareup.wire.ProtoAdapter.BYTES",
+                  empty = "okio.ByteString.EMPTY",
+                  resultType = "okio.ByteString"
                 )
               case t"String" ⇒
                 ProtoType(
-                  s"if(prep_$propName.nonEmpty){ val item = prep_$propName; ",
-                  "com.squareup.wire.ProtoAdapter.STRING", "\"\"", "String"
+                  encodeStatement = (s"if(prep_$propName.nonEmpty)", s"prep_$propName)"),
+                  serializerType = "com.squareup.wire.ProtoAdapter.STRING",
+                  empty = "\"\"",
+                  resultType = "String"
                 )
               case t"Option[${Type.Name(name)}]" ⇒
                 ProtoType(
-                  s"if(prep_$propName.nonEmpty){ val item = prep_$propName.get; ",
-                  s"${name}ProtoAdapter", "None", s"Option[$name]",
+                  encodeStatement = (s"if(prep_$propName.nonEmpty)", s"prep_$propName.get)"),
+                  serializerType = s"${name}ProtoAdapter",
+                  empty = "None",
+                  resultType = s"Option[$name]",
                   reduce=("Option(", ")")
                 )
               case t"List[${Type.Name(name)}]" ⇒
                 ProtoType(
-                  s"prep_$propName.foreach{ item => ",
-                  s"${name}ProtoAdapter", "Nil", s"List[$name]",
-                  reduce=("", s":: prep_$propName"), resultFix=s"prep_$propName = prep_$propName.reversed"
+                  encodeStatement = (s"prep_$propName.foreach(item => ","item))"),
+                  serializerType = s"${name}ProtoAdapter",
+                  empty = "Nil",
+                  resultType = s"List[$name]",
+                  resultFix = s"prep_$propName.reverse",
+                  reduce = ("", s":: prep_$propName")
                 )
               case t"Option[BigDecimal] @scale(${Lit(scale:Int)})" ⇒
                 val name = "BigDecimal"
                 ProtoType(
-                  s"if(prep_$propName.nonEmpty){ val item = prep_$propName.get; ",
-                  s"${name}ProtoAdapter", "None", s"Option[$name]",
+                  encodeStatement =
+                    (s"if(prep_$propName.nonEmpty)", s"prep_$propName.get)"),
+                  serializerType = s"${name}ProtoAdapter",
+                  empty = "None",
+                  resultType = s"Option[$name]",
                   reduce=("Option(", ")")
                 )
               /*
@@ -106,31 +115,32 @@ class schema extends scala.annotation.StaticAnnotation {
             }
             ProtoProp(
                 defArg = s"$propName: ${pt.resultType}",
-                sizeStatement = s"${pt.encodeStatement} res += ${pt.serializerType}.encodedSizeWithTag($id, item)}",
-                encodeStatement = s"${pt.encodeStatement} ${pt.serializerType}.encodeWithTag(writer, $id, item)}",
+                sizeStatement = s"${pt.encodeStatement._1} res += ${pt.serializerType}.encodedSizeWithTag($id, ${pt.encodeStatement._2}",
+                encodeStatement = s"${pt.encodeStatement._1} ${pt.serializerType}.encodeWithTag(writer, $id, ${pt.encodeStatement._2}",
                 initDecodeStatement = s"var prep_$propName: ${pt.resultType} = ${pt.empty}",
                 decodeCase = s"case $id => prep_$propName = ${pt.reduce._1} ${pt.serializerType}.decode(reader) ${pt.reduce._2}",
                 constructArg = s"prep_$propName",
-                resultFix = if(pt.resultFix.nonEmpty) s"prep_$propName = ${pt.resultFix}" else "",
-                isSystem = pt.isSystem
+                resultFix = if(pt.resultFix.nonEmpty) s"prep_$propName = ${pt.resultFix}" else ""
             )
         }.toList
-        val classImpl = if(props.exists(_.isSystem)) "" else
-          s"""case class ${messageName}Impl(${props.map(_.defArg).mkString(",")}) extends $messageName"""
-        val struct = s"""${messageName}Impl(${props.map(_.constructArg).mkString(",")})"""
+        val Sys = "Sys(.*)".r
+        val resultType = messageName match { case Sys(v) ⇒ v case v ⇒ v }
+        val classImpl = if(resultType != messageName) "" else
+          s"""case class ${resultType}Impl(${props.map(_.defArg).mkString(",")}) extends $resultType"""
+        val struct = s"""${resultType}Impl(${props.map(_.constructArg).mkString(",")})"""
         val adapterImpl = s"""
-          object ${messageName}ProtoAdapter extends com.squareup.wire.ProtoAdapter[$messageName](
+          object ${resultType}ProtoAdapter extends com.squareup.wire.ProtoAdapter[$resultType](
             com.squareup.wire.FieldEncoding.LENGTH_DELIMITED,
-            classOf[$messageName]
-          ) ${id.map(_⇒"with ProtoAdapterWithId").getOrElse("")} {
-            ${id.map(i⇒s"def id = $i").getOrElse("")}
-            def encodedSize(value: $messageName): Int = {
+            classOf[$resultType]
+          ) ${protoMods.id.map(_⇒"with ProtoAdapterWithId").getOrElse("")} {
+            ${protoMods.id.map(i⇒s"def id = $i").getOrElse("")}
+            def encodedSize(value: $resultType): Int = {
               val $struct = value
               var res = 0;
               ${props.map(_.sizeStatement).mkString("\n")}
               res
             }
-            def encode(writer: com.squareup.wire.ProtoWriter, value: $messageName) = {
+            def encode(writer: com.squareup.wire.ProtoWriter, value: $resultType) = {
               val $struct = value
               ${props.map(_.encodeStatement).mkString("\n")}
             }
@@ -149,18 +159,16 @@ class schema extends scala.annotation.StaticAnnotation {
             }
           }
         """
-        val res = ProtoMessage(s"${messageName}ProtoAdapter", classImpl, adapterImpl)
-        println(res)
-        res
-
+        val regAdapter = protoMods.id.map(_⇒s"${resultType}ProtoAdapter").getOrElse("")
+        ProtoMessage(regAdapter, classImpl, adapterImpl) :: Nil
     }.toList
     val res = q"""
       object $objectName {
         ..$stats;
-        ..${messages.map(_.classImpl.parse[Stat].get)};
+        ..${messages.map(_.classImpl).filter(_.nonEmpty).map(_.parse[Stat].get)};
         ..${messages.map(_.adapterImpl.parse[Stat].get)};
         def adapters: List[com.squareup.wire.ProtoAdapter[_<:Object] with ProtoAdapterWithId] =
-          List(..${messages.map(_.adapterName.parse[Term].get)})
+          List(..${messages.map(_.adapterName).filter(_.nonEmpty).map(_.parse[Term].get)})
       }"""
     println(res)
     res
