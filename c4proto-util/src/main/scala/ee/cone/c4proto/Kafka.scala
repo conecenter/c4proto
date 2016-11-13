e0b1125ddc5327d0966863c01b5d5deb93e04c5f
Sergei Kirjanov <rewlad@gmail.com>
2016-11-14 00:07:42 +0200

    -

--- c4proto-util/src/main/scala/ee/cone/c4proto/Kafka.scala
+++ c4proto-util/src/main/scala/ee/cone/c4proto/Kafka.scala
@@ -1,17 +1,20 @@
 package ee.cone.c4proto
 
+import java.util
+import java.util.Collections
 import java.util.concurrent.Future
 import java.util.concurrent.atomic.AtomicBoolean
 
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, RecordMetadata}
 import org.apache.kafka.common.serialization.ByteArraySerializer
-import org.apache.kafka.clients.consumer.{ConsumerRecord, KafkaConsumer}
+import org.apache.kafka.clients.consumer.{ConsumerRecord, KafkaConsumer, OffsetAndMetadata, OffsetCommitCallback}
 import org.apache.kafka.common.TopicPartition
 import org.apache.kafka.common.serialization.ByteArrayDeserializer
 
 import scala.collection.JavaConverters.seqAsJavaListConverter
 import scala.collection.JavaConverters.mapAsJavaMapConverter
 import scala.collection.JavaConverters.iterableAsScalaIterableConverter
+import scala.concurrent.Promise
 
 @protocol object KafkaProtocol extends Protocol {
   @Id(0x0010) case class TopicKey(@Id(0x0011) srcId: String, @Id(0x0012) valueTypeId: Long)
@@ -53,7 +56,7 @@ object Producer {
   }
 }
 
-class ToIdempotentConsumer(bootstrapServers: String, groupId: String, topics: List[String])(
+class SyncConsumer(bootstrapServers: String, groupId: String, topic: String)(
   val handle: ConsumerRecord[Array[Byte],Array[Byte]]⇒Unit
 ) extends Consumer {
   protected lazy val props: Map[String, Object] = Map(
@@ -61,9 +64,15 @@ class ToIdempotentConsumer(bootstrapServers: String, groupId: String, topics: Li
     "enable.auto.commit" → "false",
     "group.id" → groupId //?
   )
-  protected def assign(): Unit =
-    consumer.assign(topics.map(new TopicPartition(_, 0)).asJava)
-  protected def commit(): Unit = consumer.commitAsync() //! if single fails then all recent will be re-consumed
+  protected def runInner(): Unit = {
+    val topicPartition = new TopicPartition(topic, 0)
+    consumer.assign(List(topicPartition).asJava)
+    while(alive.get)
+      consumer.poll(1000 /*timeout*/).asScala.foreach{ rec ⇒
+        handle(rec)
+        consumer.commitSync(Collections.singletonMap(topicPartition, new OffsetAndMetadata(rec.offset + 1)))
+      }
+  } //! if consumer.commitSync() after loop, if single fails then all recent will be re-consumed
 }
 
 class ToStoredConsumer(bootstrapServers: String, topic: String, pos: Long)(
@@ -73,35 +82,38 @@ class ToStoredConsumer(bootstrapServers: String, topic: String, pos: Long)(
     "bootstrap.servers" → bootstrapServers,
     "enable.auto.commit" → "false"
   )
-  protected def assign(): Unit = {
+  lazy val ready: Promise[Unit] = Promise()
+  protected def runInner(): Unit = {
     val topicPartition = new TopicPartition(topic, 0)
     consumer.assign(List(topicPartition).asJava)
+    var readyFlag = false
+    var untilPos = consumer.position(topicPartition)
     consumer.seek(topicPartition, pos)
+    while(alive.get){
+      if(!readyFlag && untilPos <= consumer.position(topicPartition)){
+        readyFlag = true
+        ready.success()
+      }
+      consumer.poll(1000 /*timeout*/).asScala.foreach(handle)
+    }
   }
-  protected def commit(): Unit = ()
 }
 
 abstract class Consumer extends Runnable {
   protected def props: Map[String, Object]
-  protected def assign(): Unit
+  protected def runInner(): Unit
   private lazy val deserializer = new ByteArrayDeserializer
   protected lazy val consumer = new KafkaConsumer[Array[Byte], Array[Byte]](
     props.asJava, deserializer, deserializer
   )
-  protected def handle: ConsumerRecord[Array[Byte],Array[Byte]]⇒Unit
-  protected def commit(): Unit
-  private lazy val alive = new AtomicBoolean(true)
+  protected lazy val alive = new AtomicBoolean(true)
   def run(): Unit = {
     try {
       OnShutdown{() ⇒
         alive.set(false)
         consumer.wakeup()
       }
-      assign()
-      while(alive.get) {
-        consumer.poll(1000 /*timeout*/).asScala.foreach(handle)
-        commit()
-      }
+      runInner()
     } finally {
       consumer.close()
     }
