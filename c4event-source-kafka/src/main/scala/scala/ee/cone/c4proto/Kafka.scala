a7c1eda5aeedeed7cf54371e715a66105c860256
Sergei Kirjanov <rewlad@gmail.com>
2016-12-12 17:02:31 +0200

    draft

--- c4event-source-kafka/src/main/scala/scala/ee/cone/c4proto/Kafka.scala
+++ c4event-source-kafka/src/main/scala/scala/ee/cone/c4proto/Kafka.scala
@@ -1,9 +1,11 @@
 package ee.cone.c4proto
 
+import java.lang.Long
+import java.util
 import java.util.concurrent.{ExecutorService, Executors, Future, TimeUnit}
-import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.{AtomicBoolean, AtomicReference}
 
-import ee.cone.c4proto.Types.World
+import ee.cone.c4proto.Types.{Index, SrcId, World}
 import org.apache.kafka.clients.producer.{KafkaProducer, Producer, ProducerRecord, RecordMetadata}
 import org.apache.kafka.common.serialization.ByteArraySerializer
 import org.apache.kafka.clients.consumer.{Consumer, ConsumerRecord, KafkaConsumer}
@@ -13,6 +15,8 @@ import org.apache.kafka.common.serialization.ByteArrayDeserializer
 import scala.collection.JavaConverters.seqAsJavaListConverter
 import scala.collection.JavaConverters.mapAsJavaMapConverter
 import scala.collection.JavaConverters.iterableAsScalaIterableConverter
+import scala.collection.JavaConverters.mapAsScalaMapConverter
+import scala.collection.immutable.Map
 
 trait KafkaProducerApp extends ToStartApp {
   def bootstrapServers: String
@@ -83,63 +87,33 @@ class KafkaQConsumerRecordAdapter(parentStreamKey: StreamKey, rec: ConsumerRecor
   def value: Array[Byte] = rec.value
 }
 
-//val offset = new OffsetAndMetadata(rec.offset + 1)
-//consumer.commitSync(Collections.singletonMap(topicPartition, offset))
 
-class ToIdempotentConsumer(bootstrapServers: String, groupId: String, val streamKey: StreamKey)(
+
+
+
+case object ErrorsKey extends WorldKey[Index[SrcId,String]](Map.empty)
+
+class KafkaActor(bootstrapServers: String, id: String)(
+  qMessages: QMessages, treeAssembler: TreeAssembler,
   qMessageMapper: QMessageMapper, rawQSender: KafkaRawQSender
-) extends KConsumer {
-  protected lazy val props: Map[String, Object] = Map(
-    "bootstrap.servers" → bootstrapServers,
-    "enable.auto.commit" → "false",
-    "group.id" → groupId //?
-  )
-  protected def runInner(consumer: Consumer[Array[Byte], Array[Byte]], topicPartition: TopicPartition): Unit = {
-    poll(consumer){ recs ⇒
-      val toSend = recs.flatMap(qMessageMapper.mapMessage(streamKey,_)).toList
-      val metadata = toSend.map(rawQSender.sendStart)
-      metadata.foreach(_.get())
-      consumer.commitSync()
-      //! if consumer.commitSync() after loop, if single fails then all recent will be re-consumed
-    }
-  }
-}
+) extends Executable with WorldProvider with ShouldStartEarly {
+  protected def streamKey: StreamKey
 
-class ToStoredConsumer(bootstrapServers: String, val streamKey: StreamKey, pos: Long)(
-  qMessages: QMessages, treeAssembler: TreeAssembler
-) extends KConsumer with WorldProvider with ShouldStartEarly {
   private lazy val ready = new AtomicBoolean(false)
   def isReady: Boolean = ready.get()
-  protected def props: Map[String, Object] = Map(
-    "bootstrap.servers" → bootstrapServers,
-    "enable.auto.commit" → "false"
-  )
-  private var theWorld: World = Map()
-  def world: World = synchronized{ theWorld }
-  protected def runInner(consumer: Consumer[Array[Byte], Array[Byte]], topicPartition: TopicPartition): Unit = {
-    var untilPos = consumer.position(topicPartition)
-    //println("untilPos",untilPos)
-    consumer.seek(topicPartition, pos)
-    poll(consumer){ recs ⇒
-      val diff = qMessages.toTree(recs)
-      val nextWorld = treeAssembler.replace(world, diff)
-      synchronized{ theWorld = nextWorld }
-      if(!ready.get() && untilPos <= consumer.position(topicPartition) )
-        ready.set(true)
-    }
-  }
-}
-
-abstract class KConsumer extends Executable {
-  protected def streamKey: StreamKey
-  protected def props: Map[String, Object]
-  protected def runInner(consumer: Consumer[Array[Byte], Array[Byte]], topicPartition: TopicPartition): Unit
   protected lazy val alive = new AtomicBoolean(true)
-  protected def poll(consumer: Consumer[Array[Byte], Array[Byte]])(recv: Iterable[QRecord]⇒Unit): Unit =
-    while(alive.get)
-      recv(consumer.poll(1000 /*timeout*/).asScala.map(new KafkaQConsumerRecordAdapter(streamKey,_)))
-  def run(ctx: ExecutionContext): Unit = {
+  private def poll(consumer: Consumer[Array[Byte], Array[Byte]]) =
+      consumer.poll(1000 /*timeout*/).asScala
+  private val worldRef = new AtomicReference[World](Map())
+  def world: World = worldRef.get
+  type BConsumer = Consumer[Array[Byte], Array[Byte]]
+  private def initConsumer(ctx: ExecutionContext): BConsumer = {
     val deserializer = new ByteArrayDeserializer
+    val props: Map[String, Object] = Map(
+      "bootstrap.servers" → bootstrapServers,
+      "enable.auto.commit" → "false",
+      "group.id" → id //?pos
+    )
     val consumer = new KafkaConsumer[Array[Byte], Array[Byte]](
       props.asJava, deserializer, deserializer
     )
@@ -147,11 +121,73 @@ abstract class KConsumer extends Executable {
       alive.set(false)
       consumer.wakeup()
     }
+    consumer
+  }
+  private def checkInterrupt() =
+    if(Thread.interrupted || !alive.get) throw new InterruptedException
+
+  private def reduce(world: World, recs: List[QRecord]): World = {
+    val diff = qMessages.toTree(recs)
+    treeAssembler.replace(world, diff)
+  }
+  private def reduceCheck(state: (World,List[QRecord]), rec: QRecord) = try {
+    val (prevWorld, prevToSend) = state
+    val toSend = qMessageMapper.mapMessage(streamKey,rec).toList
+    val world = reduce(prevWorld, toSend.filter(_.streamKey==streamKey))
+    val errors = ErrorsKey.of(world)
+    if(errors.nonEmpty) throw new Exception(errors.toString)
+    (world, toSend ::: prevToSend)
+  } catch {
+    case e: Exception ⇒
+      val (prevWorld, prevToSend) = state
+      // ??? exception to record
+      (prevWorld, /**/prevToSend)
+  }
+
+  private def recoverWorld(consumer: BConsumer, part: List[TopicPartition]): World = {
+    var until = Single(consumer.endOffsets(part.asJava).asScala.values.toList)
+    //?hang
+    consumer.seekToBeginning(part.asJava)
+    var currentWorld: World = Map()
+    while(consumer.position(Single(part)) < until) {
+      checkInterrupt()
+      currentWorld = reduce(currentWorld, poll(consumer).map(new KafkaQConsumerRecordAdapter(streamKey,_)).toList)
+    }
+    currentWorld
+  }
+  def run(ctx: ExecutionContext): Unit = {
+    val consumer = initConsumer(ctx)
     try {
-      if(streamKey.from.isEmpty) throw new Exception(s"no source: $streamKey")
-      val topicPartition = new TopicPartition(streamKey.from, 0)
-      consumer.assign(List(topicPartition).asJava)
-      runInner(consumer, topicPartition)
+      val inboxTopicPartition = List(new TopicPartition(s"$id.inbox", 0))
+      val stateTopicPartition = List(new TopicPartition(s"$id.state", 0))
+      consumer.assign((inboxTopicPartition ::: stateTopicPartition).asJava)
+      consumer.pause(inboxTopicPartition.asJava)
+      worldRef.set(recoverWorld(consumer, stateTopicPartition))
+      consumer.pause(stateTopicPartition.asJava)
+      consumer.resume(inboxTopicPartition.asJava)
+      ready.set(true)
+      while(true){
+        checkInterrupt()
+        poll(consumer).toList match {
+          case Nil ⇒ ()
+          case recs ⇒
+            .map(new KafkaQConsumerRecordAdapter(streamKey,_))
+
+            val (nextWorld, toSend) = ((worldRef.get,Nil:List[QRecord]) /: recs)(reduceCheck)
+            val metadata = toSend.map(rawQSender.sendStart)
+            metadata.foreach(_.get())
+            worldRef.set(nextWorld)
+            val offset = new OffsetAndMetadata(recs.last.offset + 1)
+        }
+
+
+        consumer.commitSync()
+
+
+        //consumer.commitSync(Collections.singletonMap(topicPartition, offset))
+
+        //! if consumer.commitSync() after loop, if single fails then all recent will be re-consumed
+      }
     } finally {
       consumer.close()
     }
