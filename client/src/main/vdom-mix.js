bf8503f83ae10087d1cd0c1d5f2b51008a889da1
AKU3 <a.kuzmin@cone.ee>
2017-02-01 11:13:27 +0200

    metro fix 0.8

--- client/src/main/vdom-mix.js
+++ client/src/main/vdom-mix.js
@@ -5,13 +5,13 @@ import VDomClicks    from "../main/vdom-clicks"
 import VDomChanges   from "../main/vdom-changes"
 import DiffPrepare   from "../main/diff-prepare"
 
-export default function VDomMix(feedback,transforms){
+export default function VDomMix(feedback,transformsList){
     const sender = VDomSender(feedback)
     const clicks = VDomClicks(sender)
     const changes = VDomChanges(sender, DiffPrepare)
     const vDom = VDom(document.body,
-        transforms.concat(clicks.transforms).concat(changes.transforms)
+        transformsList.concat(clicks.transforms).concat(changes.transforms)
     )
-    const receivers = [].concat(vDom.receivers).concat(changes.receivers)
-    return {receivers}
+    const receiversList = [].concat(vDom.receivers).concat(changes.receivers)
+    return {receiversList}
 }
