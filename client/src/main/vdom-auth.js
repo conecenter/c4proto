5663a3b8348aebedfafbe25bed3e0fc56e9c642a
Sergei Kirjanov <rewlad@gmail.com>
2017-03-20 16:57:32 +0200

    auth draft

---
+++ client/src/main/vdom-auth.js
@@ -0,0 +1,22 @@
+
+import React from 'react'
+
+export default function VDomAuth(feedback,localStore){
+/*
+    const ChangePassword = React.createClass({
+        render: function () {
+            const onChange = event => this.setState({value: event.target.value});
+            React.createElement("div",{},[
+                React.createElement("input",{onChange:handleChange("0"),value},null),
+                React.createElement("input",{onChange:handleChange("1"),value},null)
+            ])
+        }
+    });
+
+    const transforms= {
+        tp: {
+            SignIn,ChangePassword
+        }
+    };
+    return ({transforms});*/
+}
