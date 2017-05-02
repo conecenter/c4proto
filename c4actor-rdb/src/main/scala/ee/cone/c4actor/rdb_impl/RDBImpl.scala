0a9d130e4522c49df6c506f0b4b52a1dfacdf9e2
Sergei Kirjanov <rewlad@gmail.com>
2017-05-02 12:51:41 +0300

    bolean and instant draft

--- c4actor-rdb/src/main/scala/ee/cone/c4actor/rdb_impl/RDBImpl.scala
+++ c4actor-rdb/src/main/scala/ee/cone/c4actor/rdb_impl/RDBImpl.scala
@@ -93,11 +93,50 @@ object ToExternalDBTypes {
   }
 }
 
-
-import DateToInstant._
-object DateToInstant {
+object RDBTypes {
   val msPerDay: Int = 24 * 60 * 60 * 1000
   val epoch = "to_date('19700101','yyyymmdd')"
+  //
+  private def bind(code: String, v: Object) = (code,Option(v)) :: Nil
+  def toStatement(p: Any): List[(String,Option[Object])] = {
+    case v: String ⇒ bind("?",v)
+    case v: java.lang.Boolean ⇒ bind("?",v)
+    case v: java.lang.Long ⇒ bind("?",v)
+    case v: BigDecimal ⇒ bind("?",v.bigDecimal)
+    case v: Instant ⇒ bind(s"$epoch + (?/$msPerDay)",new java.lang.Long(v.toEpochMilli))
+  }
+  //
+  def sysTypes: List[String] = List(
+    classOf[String],
+    classOf[java.lang.Boolean],
+    classOf[java.lang.Long],
+    classOf[BigDecimal],
+    classOf[Instant]
+  ).map(_.getName.split("\\.").last)
+  //
+  def constructorToTypeArg(tp: String): String⇒String = tp match {
+    case "Boolean" ⇒ a ⇒ s"case when $a then 'T' else null end"
+    case at if at.endsWith("s") ⇒  a ⇒ s"case when $a is null then $at() else $a end"
+    case _ ⇒ a ⇒ a
+  }
+  def encodeExpression(tp: String): String⇒String = tp match {
+    case "Instant" ⇒ a ⇒ s"round(($a - $epoch) * $msPerDay)"
+    case "Boolean" ⇒ a ⇒ s"(case when $a then 'T' else '' end)"
+    case "String"|"Long"|"BigDecimal" ⇒ a ⇒ a
+  }
+  def toUniversalProp(tag: Int, typeName: String, value: String): UniversalProp = typeName match {
+    case "String" ⇒
+      UniversalPropImpl[String](tag,value)(ProtoAdapter.STRING)
+    case "Boolean" ⇒
+      UniversalPropImpl[java.lang.Boolean](tag,value.nonEmpty)(ProtoAdapter.BOOL)
+    case "Long" | "Instant" ⇒
+      UniversalPropImpl[java.lang.Long](tag,java.lang.Long.parseLong(value))(ProtoAdapter.SINT64)
+    case "BigDecimal" ⇒
+      val BigDecimalFactory(scale,bytes) = BigDecimal(value)
+      val scaleProp = UniversalPropImpl(0x0001,scale:Integer)(ProtoAdapter.SINT32)
+      val bytesProp = UniversalPropImpl(0x0002,bytes)(ProtoAdapter.BYTES)
+      UniversalPropImpl(tag,UniversalNode(List(scaleProp,bytesProp)))(UniversalProtoAdapter)
+  }
 }
 
 
@@ -108,18 +147,13 @@ case class ToExternalDBTx(srcId: SrcId, from: Option[HasState], to: Option[HasSt
     (name,None) :: args.zipWithIndex.flatMap{
       case(el,idx) ⇒ (if(idx==0) "(" else ",", None) :: el
     } ::: (")",None) :: Nil
-  private def bind(code: String, v: Object) = (code,Option(v)) :: Nil
+
   private def commonName(args: List[List[Part]]) =
     Single(args.map{
       case (name,None) :: _ ⇒ name
       case _ ⇒ throw new Exception
     }.filter(_!="null").distinct)
   private def toStatement(p: Any): List[Part] = p match {
-    case v: String ⇒ bind("?",v)
-    case v: java.lang.Boolean ⇒ bind("?",v)
-    case v: java.lang.Long ⇒ bind("?",v)
-    case v: BigDecimal ⇒ bind("?",v)
-    case v: Instant ⇒ bind(s"$epoch + (?/$msPerDay)",new java.lang.Long(v.toEpochMilli))
     case Nil | None ⇒ ("null",None) :: Nil
     case Some(e) ⇒ toStatement(e)
     case l: List[_] ⇒
@@ -128,6 +162,7 @@ case class ToExternalDBTx(srcId: SrcId, from: Option[HasState], to: Option[HasSt
     case p: Product ⇒
       val name = s"b${p.productPrefix.split("\\$").last}"
       function(name, p.productIterator.map(toStatement).toList)
+    case e ⇒ RDBTypes.toStatement(e)
   }
 
   def transform(local: World): World = WithJDBCKey.of(local){ conn ⇒
@@ -180,8 +215,10 @@ case class FromExternalDBSyncTransform(srcId:SrcId) extends TxTransform {
       val textEncoded = messages.map(_("content") match {
         case c: java.sql.Clob ⇒ c.getSubString(1,toIntExact(c.length()))
       }).mkString
-      val parser = new IndentedParser(' ',"\n",StringToUniversalPropImpl.converters.toMap)
-      val universalNode = parser.parse(textEncoded)
+      val lineSplitter = "\n"
+      val parser = new IndentedParser(' ',lineSplitter,RDBTypes.toUniversalProp)
+      val lines = textEncoded.split(lineSplitter).filter(_.nonEmpty).toList
+      val universalNode = UniversalNode(parser.parseProps(lines, Nil))
       //println(PrettyProduct.encode(universalNode))
       val updateList = universalNode.props.map{ prop ⇒
         val srcId = prop.value match {
@@ -222,37 +259,15 @@ object FinallyClose {
   def apply[A<:AutoCloseable,T](o: A)(f: A⇒T): T = try f(o) finally o.close()
 }
 
-abstract class RDBType[T](val theClass: Class[T]) {
-  def bind(stm: PreparedStatement, pos: Int, value: T): Unit
-
-}
-class StringRDBType extends RDBType(classOf[String]) {
-  def bind(stm: PreparedStatement, pos: Int, value: String): Unit =
-    stm.setString(pos, value)
-}
-class BigDecimalRDBType extends RDBType(classOf[BigDecimal]) {
-  def bind(stm: PreparedStatement, pos: Int, value: BigDecimal): Unit =
-    stm.setBigDecimal(pos, value.bigDecimal)
-}
-class LongRDBType extends RDBType(classOf[java.lang.Long]) {
-  def bind(stm: PreparedStatement, pos: Int, value: java.lang.Long): Unit =
-    stm.setLong(pos, value)
-}
-class BooleanRDBType extends RDBType(classOf[java.lang.Boolean]) {
-  def bind(stm: PreparedStatement, pos: Int, value: java.lang.Boolean): Unit =
-    stm.setBoolean(pos, value)
-}
-
-
 class RConnectionImpl(conn: java.sql.Connection) extends RConnection {
   private def bindObjects(stmt: java.sql.PreparedStatement, bind: List[Object]) =
-  //bind.zipWithIndex.foreach{ case (v,i) ⇒ stmt.setObject(i+1,v) }
-    bind.zipWithIndex.foreach{
+    bind.zipWithIndex.foreach{ case (v,i) ⇒ stmt.setObject(i+1,v) }
+      /*
       case (v:String, i:Int) ⇒ stmt.setString(i+1, v)
       case (v:java.lang.Boolean, i:Int) ⇒ stmt.setBoolean(i+1, v)
       case (v:java.lang.Long, i:Int) ⇒ stmt.setLong(i+1, v)
-      case (v:BigDecimal, i:Int) ⇒ stmt.setBigDecimal(i+1, v.bigDecimal)
-    }
+      case (v:BigDecimal, i:Int) ⇒ stmt.setBigDecimal(i+1, v.bigDecimal)*/
+
   def execute(code: String, bind: List[Object]): Unit =
     FinallyClose(conn.prepareStatement(code)){ stmt ⇒
       bindObjects(stmt, bind)
@@ -299,13 +314,11 @@ class DDLGeneratorImpl(
   }
   protected def bodyStatements(statements: List[String]): String =
     s"begin\n${statements.map(l⇒s"  $l\n").mkString("")}end;"
+  private def returnIfNull = "if rec is null then return ''; end if;"
   private def esc(handler: String, expr: String) =
-    bodyStatements(s"return chr(10) || key || ' ' || '$handler' || replace($expr, chr(10), chr(10)||' ');" :: Nil)
+    bodyStatements(returnIfNull :: s"return chr(10) || key || ' ' || '$handler' || replace($expr, chr(10), chr(10)||' ');" :: Nil)
   private def getId(i: Long) = "'0x%04x'".format(i)
   private def longStrType = toDbName("String","t",1)
-  private def isSysType(name: String) =
-    name == toDbName("String","t").toLowerCase ||
-      name == toDbName("BigDecimal","t").toLowerCase
   private def encoderName(dType: String) = toDbName(dType,"e")
   private def encode(name: String, body: String) =
     hooks.function(encoderName(name),s"key ${toDbName("String","t")}, ${recArg(name)}",longStrType,body)
@@ -338,7 +351,11 @@ class DDLGeneratorImpl(
         s"${encoderName(prop.resultType)}(${getId(prop.id)}, rec.a${prop.propName})"
       }.mkString(" || ")
       val encodeNode = encode(name, esc("Node", encodeArgs))
-      val encodeNodes = encode(names, s"res $longStrType;\n${bodyStatements(List(hooks.loop(encoderName(name)),"return res;"))}")
+      val encodeNodes = encode(names, s"res $longStrType;\n${bodyStatements(List(
+        returnIfNull,
+        hooks.loop(encoderName(name)),
+        "return res;"
+      ))}")
       //
       val encodeExtOut =
         for(n ← List(name) if isFromSql)
@@ -365,11 +382,7 @@ class DDLGeneratorImpl(
         s"create type $tNames as table of $tName;"
       ))
       val constructorArgs = props.map(prop ⇒ s"a${prop.propName} ${toDbName(prop.resultType, "t")} default null").mkString(", ")
-      val defaultArgs = attrs.map{
-        case (a,"Boolean") ⇒ s"case when $a then 'T' else null end"
-        case (a,at) if at.endsWith("s") ⇒  s"case when $a is null then $at() else $a end"
-        case (a,_) ⇒ a
-      }.mkString(", ")
+      val defaultArgs = attrs.map{case (n,t)⇒ RDBTypes.constructorToTypeArg(t)(n)}.mkString(", ")
       val constructor = hooks.function(toDbName(name, "b"), constructorArgs, tName, bodyStatements(List(s"return $tName($defaultArgs);")))
 
       //
@@ -388,8 +401,7 @@ class DDLGeneratorImpl(
     //
     val needFunctions =
       options.collect{ case o: NeedCodeDBOption ⇒ o.needCode } :::
-        encode("Instant", esc("Long", s"chr(10) || round((rec - $epoch) * $msPerDay)")) ::
-        List("String","Long","BigDecimal").map(n⇒encode(n, esc(n, "chr(10) || rec"))) :::
+        RDBTypes.sysTypes.map(t⇒encode(t, esc(t, s"chr(10) || ${RDBTypes.encodeExpression(t)("rec")}"))) :::
         needs.collect{ case f: NeedCode ⇒ f }
     val needFunctionNames = uniqueBy(needFunctions)(_.drop).keySet
     val replaceFunctions =
@@ -400,6 +412,7 @@ class DDLGeneratorImpl(
   }
   private def orderedTypes(needTypesList: List[DropType]): (List[DropType],Set[DropType]) = {
     val needTypes = uniqueBy(needTypesList)(_.name)
+    val isSysType = RDBTypes.sysTypes.map(t⇒toDbName(t,"t").toLowerCase).toSet
     def regType(res: ReverseInsertionOrder[String,DropType], name: String): ReverseInsertionOrder[String,DropType] = {
       if(res.map.contains(name)) res else {
         val needType = needTypes(name)
@@ -446,21 +459,17 @@ object UniversalProtoAdapter extends ProtoAdapter[UniversalNode](FieldEncoding.L
 
 class IndentedParser(
   splitter: Char, lineSplitter: String,
-  propTypeRegistry: String⇒StringToUniversalProp.Converter
+  toUniversalProp: (Int,String,String)⇒UniversalProp
 ) {
-  def parse(data: String): UniversalNode = {
-    val lines = data.split(lineSplitter).filter(_.nonEmpty).toList
-    UniversalNode(parseProps(lines, Nil))
-  }
   //@tailrec final
   private def parseProp(key: String, value: List[String]): UniversalProp = {
     val Array(xHex,handlerName) = key.split(splitter)
     val ("0x", hex) = xHex.splitAt(2)
     val tag = Integer.parseInt(hex, 16)
-    if(handlerName != "Node") propTypeRegistry(handlerName)(tag,value.mkString(lineSplitter))
+    if(handlerName != "Node") toUniversalProp(tag,handlerName,value.mkString(lineSplitter))
     else UniversalPropImpl(tag,UniversalNode(parseProps(value, Nil)))(UniversalProtoAdapter)
   }
-  private def parseProps(lines: List[String], res: List[UniversalProp]): List[UniversalProp] =
+  def parseProps(lines: List[String], res: List[UniversalProp]): List[UniversalProp] =
     if(lines.isEmpty) res.reverse else {
       val key = lines.head
       val value = lines.tail.takeWhile(_.head == splitter).map(_.tail)
@@ -469,29 +478,5 @@ class IndentedParser(
     }
 }
 
-object StringToUniversalProp {
-  type Converter = (Int,String)⇒UniversalProp
-}
-
-object StringToUniversalPropImpl {
-  def string(tag: Int, value: String): UniversalProp =
-    UniversalPropImpl[String](tag,value)(ProtoAdapter.STRING)
-  def long(tag: Int, value: String): UniversalProp =
-    UniversalPropImpl[java.lang.Long](tag,java.lang.Long.parseLong(value))(ProtoAdapter.SINT64)
-  def bigDecimal(tag: Int, value: String): UniversalProp = {
-    val BigDecimalFactory(scale,bytes) = BigDecimal(value)
-    val scaleProp = UniversalPropImpl(0x0001,scale:Integer)(ProtoAdapter.SINT32)
-    val bytesProp = UniversalPropImpl(0x0002,bytes)(ProtoAdapter.BYTES)
-    UniversalPropImpl(tag,UniversalNode(List(scaleProp,bytesProp)))(UniversalProtoAdapter)
-  }
-  def boolean(tag: Int, value: String): UniversalProp =
-    UniversalPropImpl[java.lang.Boolean](tag,value.nonEmpty)(ProtoAdapter.BOOL)
-  def converters: List[(String,StringToUniversalProp.Converter)] = List(
-    "String" → string,
-    "Long" → long,
-    "BigDecimal" → bigDecimal,
-    "Boolean" → boolean
-  )
-}
 
 //protobuf universal draft
