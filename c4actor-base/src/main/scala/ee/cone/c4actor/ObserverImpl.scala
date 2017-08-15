eae714159109930e1fa581cd1a94bb7b2c3e2ed3
Sergei Kirjanov <rewlad@gmail.com>
2017-08-15 18:02:28 +0300

    0.A.4 refactoring
    World --> Context
    WorldKey --> TransientLens / SharedComponentKey
    c4assemble.Lens --> c4assemble.Getter / c4actor.Lens
    By.srcId, WithSrcId --> ByPK, WithPK
    Offset --> Firstborn
    LEvent.add --> TxAdd
    InitLocalsApp, initLocals, InitLocal --> ToInjectApp, toInject, ToInject
    
    and more internal refactoring

---
+++ c4actor-base/src/main/scala/ee/cone/c4actor/ObserverImpl.scala
@@ -0,0 +1,76 @@
+package ee.cone.c4actor
+
+import java.time.Instant
+
+import ee.cone.c4actor.Types.{SrcId, TransientMap}
+
+import scala.collection.immutable.{Map, Seq}
+
+class TxTransforms(qMessages: QMessages) {
+  type TransientTransform = TransientMap ⇒ TransientMap
+  def get(global: Context): Map[SrcId,TransientTransform] =
+    ByPK(classOf[TxTransform]).of(global).transform{ case(key,_) ⇒ handle(global,key) }
+  private def handle(global: Context, key: SrcId): TransientTransform = ((prev:TransientMap) ⇒
+    new Context(global.injected, global.assembled, prev)
+  ).andThen{ (local:Context) ⇒
+    val txTransform = ByPK(classOf[TxTransform]).of(local).get(key)
+    if(
+      txTransform.isEmpty ||
+        qMessages.worldOffset(global) < OffsetWorldKey.of(local) ||
+      Instant.now.isBefore(SleepUntilKey.of(local))
+    ) local.transient else try {
+      Trace{
+        (txTransform.get.transform(_)).andThen(qMessages.send)(local).transient
+      }
+    } catch {
+      case exception: Exception ⇒
+        println(s"Tx failed [$key][${Thread.currentThread.getName}][\n${exception.getStackTrace.map(l⇒s"  $l\n").mkString}]")
+        // exception.printStackTrace() //??? |Nil|throw
+        val was = ErrorKey.of(local)
+        Function.chain(List(
+          ErrorKey.set(exception :: was),
+          SleepUntilKey.set(Instant.now.plusSeconds(was.size))
+        ))(global).transient
+      /*case e: Throwable ⇒
+        println("Throwable0")
+        e.printStackTrace()
+        throw e*/
+    }
+  }
+}
+
+class SerialObserver(localStates: Map[SrcId,TransientMap])(
+  transforms: TxTransforms
+) extends Observer {
+  def activate(global: Context): Seq[Observer] = {
+    val nLocalStates = transforms.get(global).transform{ case(key,handle) ⇒
+      handle(localStates.getOrElse(key,Map.empty))
+    }
+    List(new SerialObserver(nLocalStates)(transforms))
+  }
+}
+
+class ParallelObserver(
+  localStates: Map[SrcId,FatalFuture[TransientMap]],
+  transforms: TxTransforms,
+  execution: Execution
+) extends Observer {
+  private def empty: FatalFuture[TransientMap] = execution.future(Map.empty)
+  def activate(global: Context): Seq[Observer] = {
+    val inProgressMap = localStates.filterNot{ case(k,v) ⇒ v.isCompleted }
+    val toAdd = transforms.get(global).transform{ case(key,handle) ⇒
+      localStates.getOrElse(key,empty).map(handle)
+    }
+    val nLocalStates = inProgressMap ++ toAdd
+    List(new ParallelObserver(nLocalStates,transforms,execution))
+  }
+}
+
+/*
+* in trans? in was? !isDone?
+*  0 0 x => -
+*  0 1 0 => del
+*  0 1 1 => keep same
+*  1 0 x => new
+*  1 1 x => chain
+* */
