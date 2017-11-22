ea30692d3416fa84c2b7e81b47615194c5470d44
Pavel Vavilov <paw@hot.ee>
2017-11-22 16:55:05 +0200

    scanner proxy

--- client/src/extra/scanner-proxy.js
+++ client/src/extra/scanner-proxy.js
@@ -7,7 +7,7 @@ export default function ScannerProxy({Scanner,setInterval,clearInterval,log,inne
 	const wifiCallbacks = [];
 	const periodicCheck = () => {
 		if(referenceCounter<=0) {
-			if(isOn) {setScannerEnable(false)}
+			if(isOn) {setScannerEnable(false,isOn.scanMode)}
 			if(interval) {
 				clearInterval(interval);
 				interval = null;
@@ -15,15 +15,33 @@ export default function ScannerProxy({Scanner,setInterval,clearInterval,log,inne
 		}		
 	}	
 	const scannerStatus = () => isOn
-	const setScannerEnable = (value) => {isOn = value; if(isOn) Scanner&&Scanner.setScannerEnable(); else Scanner&&Scanner.setScannerDisable(); log(`scanner: set ${value}`)}
+	const setScannerEnable = (value,scanMode) => {
+		isOn = value?{value,scanMode}:value;
+		switch(scanMode){
+			case "uhf": if(isOn) Scanner&&Scanner.setUHFenable(); else Scanner&&Scanner.setUHFdisable();
+			default: if(isOn) Scanner&&Scanner.setScannerEnable() else Scanner&&Scanner.setScannerDisable();
+		}			
+		log(`scanner: set ${value} ${scanMode}`)
+	}
 	const receiveAction = (barCode) => {Object.keys(callbacks).forEach(k=>callbacks[k]("barCode",barCode)); log(callbacks);}
 	const reg = (obj) => {
 		referenceCounter += 1;
 		const key = Math.random();
 		callbacks[key] = obj.callback;
-		setScannerEnable(true);
+		setScannerEnable(true,obj.scanMode());
 		if(!interval) interval = setInterval(periodicCheck,2000);
-		return () => {referenceCounter -= 1; delete callbacks[key];log("unreg");}
+		const unreg = () => {
+			referenceCounter -= 1;
+			delete callbacks[key];
+			log("unreg");
+		}
+		const switchTo = (scanMode) => {
+			if(isOn && isOn.scanMode!=scanMode) {
+				setScannerEnable(false,isOn.scanMode)
+				setScannerEnable(true,scanMode)
+			}
+		}
+		return {unreg,switchTo}
 	}
 	const moveScrollBy = (adj)=>{
 		const maxHeight = document.querySelector("html").getBoundingClientRect().height
