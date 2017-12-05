81231ac10052d8a7cff42a73e41c7fcfad4e3387
Sergei Kirjanov <rewlad@gmail.com>
2017-12-05 15:45:24 +0200

    0.B.1 SyncTx

--- c4gate-client/src/main/scala/ee/cone/c4gate/PostConsumerImpl.scala
+++ c4gate-client/src/main/scala/ee/cone/c4gate/PostConsumerImpl.scala
@@ -1,5 +1,6 @@
 package ee.cone.c4gate
 
+import ee.cone.c4actor.SyncTx.NeedSrcId
 import ee.cone.c4actor.Types.SrcId
 import ee.cone.c4actor._
 import ee.cone.c4assemble.Types.Values
@@ -7,30 +8,22 @@ import ee.cone.c4assemble.{Assemble, assemble, by}
 import ee.cone.c4gate.AlienProtocol.PostConsumer
 
 @assemble class PostConsumerAssemble(actorName: String) extends Assemble {
-
-  type WasConsumer = SrcId
-  def wasConsumers(
-    key: SrcId,
-    consumers: Values[PostConsumer]
-  ): Values[(WasConsumer,PostConsumer)] =
-    for(c ← consumers if c.consumer == actorName) yield WithPK(c)
-
-  type NeedConsumer = SrcId
   def needConsumers(
     key: SrcId,
     consumers: Values[LocalPostConsumer]
-  ): Values[(NeedConsumer,PostConsumer)] =
+  ): Values[(NeedSrcId,PostConsumer)] =
     for(c ← consumers.distinct)
       yield WithPK(PostConsumer(s"$actorName/${c.condition}", actorName, c.condition))
-
-  def syncConsumers(
-    key: SrcId,
-    @by[WasConsumer] wasConsumers: Values[PostConsumer],
-    @by[NeedConsumer] needConsumers: Values[PostConsumer]
-  ): Values[(SrcId,TxTransform)] =
-    if(wasConsumers.toList == needConsumers.toList) Nil
-    else List(WithPK(SimpleTxTransform(key,
-      wasConsumers.flatMap(LEvent.delete) ++ needConsumers.flatMap(LEvent.update)
-    )))
 }
 
+class PostConsumerAssembles(actorName: String, syncTxFactory: SyncTxFactory)(
+  inner: List[Assemble] = List(
+    new PostConsumerAssemble(actorName),
+    syncTxFactory.create[PostConsumer](
+      classOf[PostConsumer], c ⇒ c.consumer == actorName, _ ⇒ "PostConsumerSync",
+      (key,tasks)⇒SimpleTxTransform(key,tasks.flatMap(_.events))
+    )
+  )
+) extends Assemble {
+  override def dataDependencies = f ⇒ inner.flatMap(_.dataDependencies(f))
+}
