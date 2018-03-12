63afa2442ee2ab503b1dde22813d4aba63cdd7d8
Pavel Vavilov <paw@hot.ee>
2018-03-12 10:09:32 +0200

    upd

--- client/src/extra/metro-ui.js
+++ client/src/extra/metro-ui.js
@@ -271,8 +271,8 @@ export default function MetroUi({log,sender,svgSrc,fileReader,documentManager,fo
 			const alt2 = this.props.isBurgerOpen?{transform: "rotate(45deg)"}:{}
 			const alt3 = this.props.isBurgerOpen?{opacity: "0"}:{}
 			const svg = $("svg",{xmlns:"http://www.w3.org/2000/svg","xmlnsXlink":"http://www.w3.org/1999/xlink",height:"1.5em",width:"1.8em", style:{"enableBackground":"new 0 0 32 33"}, version:"1.1", viewBox:"0 0 32 32","xmlSpace":"preserve"},[				
-				$("line",{style:{...c,...alt1},key:1,"strokeLinecap":"round",x1:this.props.isBurgerOpen?"3":"2",y1:this.props.isBurgerOpen?"17":"9",x2:"30",y2:this.props.isBurgerOpen?"17":"9","strokeWidth":"4","stroke":"white"}),							
-				$("line",{style:{...c,...alt2},key:2,"strokeLinecap":"round",x1:"2",y1:"17",x2:this.props.isBurgerOpen?"29":"30",y2:"17","strokeWidth":"4","stroke":"white"}),								
+				$("line",{style:{...c,...alt1},key:1,"strokeLinecap":"round",x1:"2",y1:this.props.isBurgerOpen?"17":"9",x2:"30",y2:this.props.isBurgerOpen?"17":"9","strokeWidth":"4","stroke":"white"}),							
+				$("line",{style:{...c,...alt2},key:2,"strokeLinecap":"round",x1:this.props.isBurgerOpen?"3":"2",y1:"17",x2:this.props.isBurgerOpen?"31":"30",y2:"17","strokeWidth":"4","stroke":"white"}),								
 				$("line",{style:{...c,...alt3},key:3,"strokeLinecap":"round",x1:"2",y1:"25",x2:"30",y2:"25","strokeWidth":"4","stroke":"white"})				
 			])					
 						
