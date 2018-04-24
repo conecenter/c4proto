7cf60593869e02f8d8cd754ed0edc77dda80d4c0
Ilya Kachalsky <ilya,kachalsky@gmail.com>
2018-04-24 23:00:28 +0300

    RegistryFix

--- c4actor-extra/src/main/scala/ee/cone/c4actor/rangers/HashSearchRangerRegistry.scala
+++ c4actor-extra/src/main/scala/ee/cone/c4actor/rangers/HashSearchRangerRegistry.scala
@@ -1,30 +1,35 @@
 package ee.cone.c4actor.rangers
 
-import ee.cone.c4actor.{Ranger, RangerWithCl}
+import ee.cone.c4actor.RangerWithCl
 
 trait HashSearchRangerRegistryApp {
-  def hashSearchRangerRegistry:HashSearchRangerRegistryApi
+  def hashSearchRangerRegistry: HashSearchRangerRegistryApi
 }
 
-trait HashSearchRangersApp{
+trait HashSearchRangersApp {
   def hashSearchRangers: List[RangerWithCl[_ <: Product, _]] = Nil
 }
 
-trait HashSearchRangerRegistryMix extends HashSearchRangerRegistryApp with HashSearchRangersApp{
+trait HashSearchRangerRegistryMix extends HashSearchRangerRegistryApp with HashSearchRangersApp {
   def hashSearchRangerRegistry: HashSearchRangerRegistryApi = HashSearchRangerRegistryImpl(hashSearchRangers)
 }
 
 trait HashSearchRangerRegistryApi {
   def getByCl[By <: Product, Field](byCl: Class[By], fieldCl: Class[Field]): Option[RangerWithCl[By, Field]]
-  def getByName[By <: Product, Field](rangerName: String): Option[Ranger[By, Field]]
+
+  def getByName[By <: Product, Field](rangerName: String): Option[RangerWithCl[By, Field]]
 }
 
 case class HashSearchRangerRegistryImpl(rangers: List[RangerWithCl[_ <: Product, _]]) extends HashSearchRangerRegistryApi {
-  lazy val rangerMap: Map[(String, String), Ranger[_ <: Product, _]] = rangers.map(ranger ⇒ (ranger.byCl.getName, ranger.fieldCl.getName) → ranger).toMap
+  lazy val rangerMap: Map[(String, String), RangerWithCl[_ <: Product, _]] =
+    rangers.map(ranger ⇒ (ranger.byCl.getName, ranger.fieldCl.getName) → ranger).toMap
 
-  lazy val byNameMap: Map[String, Ranger[_ <: Product, _]] = rangers.map(ranger ⇒ ranger.getClass.getName → ranger).toMap
+  lazy val byNameMap: Map[String, RangerWithCl[_ <: Product, _]] =
+    rangers.map(ranger ⇒ ranger.getClass.getName → ranger).toMap
 
-  def getByCl[By <: Product, Field](byCl: Class[By], fieldCl: Class[Field]): Option[Ranger[By, Field]] = rangerMap.get((byCl.getName, fieldCl.getName)).map(_.asInstanceOf[Ranger[By, Field]])
+  def getByCl[By <: Product, Field](byCl: Class[By], fieldCl: Class[Field]): Option[RangerWithCl[By, Field]] =
+    rangerMap.get((byCl.getName, fieldCl.getName)).map(_.asInstanceOf[RangerWithCl[By, Field]])
 
-  def getByName[By <: Product, Field](rangerName: String): Option[Ranger[By, Field]] = byNameMap.get(rangerName).map(_.asInstanceOf[Ranger[By, Field]])
+  def getByName[By <: Product, Field](rangerName: String): Option[RangerWithCl[By, Field]] =
+    byNameMap.get(rangerName).map(_.asInstanceOf[RangerWithCl[By, Field]])
 }
