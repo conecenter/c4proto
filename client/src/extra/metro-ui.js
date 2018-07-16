967c0583f71465304c21b62df585b0ae23a0a51e
wregs <paw@hot.ee>
2018-07-16 07:59:37 +0000

    colorpicker

--- client/src/extra/metro-ui.js
+++ client/src/extra/metro-ui.js
@@ -2804,151 +2804,29 @@ export default function MetroUi({log,requestState,svgSrc,documentManager,focusMo
 			return $("div",{style,ref,...actions},this.props.children)
 		}
 	}
-	/*
-	class FilterContainerElement extends StatefulComponent{		
-		getInitialState(){return {singleHeight:"0px",version:0}}		
-		isOverflown(){
-			if(!this.el) return;
-			const e = this.el;
-			const p = e.parentNode.getBoundingClientRect()
-			const c = Array.from(e.children)
-			
-			
-			const singleHeight = c.length>0?c[0].getBoundingClientRect().height:null			
-			if(singleHeight != this.state.singleHeight)
-				this.setState({singleHeight:singleHeight})
-			
-		}	
-		componentDidMount(){			
-			this.isOverflown()
-			checkActivateCalls.add(this.calc)
-		}
-		componentDidUpdate(prevProps){
-			this.isOverflown()
-		}
-		componentWillUnmount(){
-			checkActivateCalls.remove(this.calc)
-		}
-		calc(){
-			if(!this.el) return
-			let maxLines = 1 
-			
-			const chldMap = this.props.children.map(child=>({child,basis:this.parseBasis(child.props.incoming.at.style.flexBasis),en:child.props.incoming.at.active&&true,maxLines:1}))
-			const chldAMap = chldMap.filter(_=>_.en)
-			const pRect = this.el.getBoundingClientRect()			
-			const chldAWidth = chldAMap.reduce((a,e)=>(a+(e.en?e.basis:0)),0)
-			if(chldAWidth > pRect.width) {
-				let t = pRect.width
-				maxLines = chldAMap.reduce((a,e,i,arr)=>{					
-					if(t-e.basis<0) {t = pRect.width - e.basis;t=t>=0?t:0;return a+1}
-					if(t-e.basis>=0) {t -=e.basis;return a}
-				},1)
-				//maxLines = Math.ceil(chldAWidth/(Math.floor(pRect.width/chldMap[0].basis)*chldMap[0].basis))
-			}
-			if(chldMap[0].basis>pRect.width) {maxLines = chldAMap.length;maxLines=maxLines>0?maxLines:1}
-			let chldWidth = 0
-			do{
-				chldWidth = chldMap.reduce((a,e)=>{
-					e.maxLines = maxLines
-					return a+(e.en?e.basis:0);
-				},0)				
-				const firstPassiveIndex = chldMap.findIndex(_=>!_.child.props.incoming.at.active && !_.en)
-				if(firstPassiveIndex==-1) break;
-				const child = chldMap[firstPassiveIndex]
-				let lineWidth = Math.floor(pRect.width/child.basis)*child.basis;lineWidth = lineWidth?lineWidth:child.basis
-				
-				if(chldWidth+child.basis<=lineWidth*maxLines) {					
-					child.en=true					
-				}
-				else break;
-			}while(true)
-			
-			const same = () =>{
-				if(this.chldMap.length!=chldMap.length || !this.chldMap.every((e,i)=>e.child.key == chldMap[i].child.key && e.en == chldMap[i].en && e.maxLines == chldMap[i].maxLines && e.basis == chldMap[i].basis))
-					return false
-				return true
-			}
-		
-			if(!this.chldMap || !same()) this.update(chldMap)
-		}
-		update(chldMap){
-			this.chldMap = chldMap
-			this.setState({...this.state,version:this.state.version+1})
-		}
-		parseBasis(strBasis){
-			if(!this.emEl) return 0;
-			const emBasis = parseFloat(strBasis)
-			const pixelBasis = this.emEl.getBoundingClientRect().height * emBasis
-			return pixelBasis
-		}
-		render(){
-		//	log(`render`)
-			const children = this.props.open||!Array.isArray(this.chldMap)?this.props.children:this.chldMap.filter(_=>_.en).map(_=>this.props.children.find(c=>c.key == _.child.key))
-			const maxLines = !Array.isArray(this.chldMap)?1:this.chldMap.reduce((a,e)=>(e.maxLines>a?e.maxLines:a),1)
-			const style = {
-				backgroundColor: "#c0ced8",
-				height:this.props.open||!this.state.singleHeight?"auto":this.state.singleHeight*maxLines+"px",
-				marginBottom:"0.4em",
-				overflow:"hidden",
-				padding:".15625em",
-				width:"100%",
-				...this.props.style
-			}
-			const buttonStyle = {
-				"float":"right",
-				marginLeft:"0.6em"
-			}
-			const filterStyle = {
-				display:"flex",
-				flexWrap:"wrap"
-			}
-			const fStyle=(h) => ({
-				visibility:h?"":"hidden"
-			})
-			const imgStyle = {
-				transform:!this.props.open?"rotate(180deg)":"",
-				transition:"transform 140ms",
-				height:"1em",
-				width:"1em"
-			}	
-			
-			const arrowSvg = `<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-								 width="150px" height="150px" viewBox="0 0 451.847 451.846" style="enable-background:new 0 0 451.847 451.846;"
-								 xml:space="preserve">
-								<path d="M248.292,106.406l194.281,194.29c12.365,12.359,12.365,32.391,0,44.744c-12.354,12.354-32.391,12.354-44.744,0
-									L225.923,173.529L54.018,345.44c-12.36,12.354-32.395,12.354-44.748,0c-12.359-12.354-12.359-32.391,0-44.75L203.554,106.4
-									c6.18-6.174,14.271-9.259,22.369-9.259C234.018,97.141,242.115,100.232,248.292,106.406z"/>
-							</svg>`
-			const svgData = svgSrc(arrowSvg)
-			const img = $("img",{src:svgData,style:imgStyle})
-			const emElStyle={
-				position:"absolute",
-				top:"0",
-				zIndex:"-1",
-				height:"1em"
-			}			
-			const emRefEl = $("div",{ref:ref=>this.emEl=ref,key:"emref",style:emElStyle});
-			return $("div",{style},[
-				emRefEl,
-				$("div",{key:"buttons",style:buttonStyle},$(ButtonElement,{onClick:this.props.onClick},img)),
-				$("div",{key:"contents",style:filterStyle,ref:el=>this.el=el},children)
-			])
-		}
-	}	
-	const FilterElement = ({active,children,style}) => {		
-		return $("div",{style,className:"filterElement"},children)
-	}*/
+
 	class ColorCreator extends StatefulComponent{
 		onChange(e){
-			if(this.props.onChange)
-				this.props.onChange({target:{headers:{"X-r-action":"change"},value:e.target.value}});
+			if(this.timeout) return
+			const value = e.target.value
+			this.timeout = setTimeout(()=>{				
+				if(this.props.onChange)
+					this.props.onChange({target:{headers:{"X-r-action":"change"},value}})
+				this.timeout = null
+			},500)
+			
 		}
 		onBlur(e){
 			if(this.props.onBlur)
 				this.props.onBlur(e)
 		}
-		componentDidMount(){				
-			this.drawCanvasImage();//camvas.imcludeImage(...)
+		componentDidMount(){			
+			//this.el.addEventListener("input",this.onChange,false)
+			//this.drawCanvasImage();//camvas.imcludeImage(...)
+		}
+		componentWillUnmount(){
+			this.timeout = null
+			//this.el.removeEventListener("input",this.onChange,false)
 		}
 		drawCanvasImage(){
 			if(this.canvasEl) {
@@ -2993,6 +2871,7 @@ export default function MetroUi({log,requestState,svgSrc,documentManager,focusMo
 			};
 			const inputStyle={
 				width:'10em',
+				border:"none",
 				...this.props.style
 			};
 			const canvasStyle ={
@@ -3000,12 +2879,7 @@ export default function MetroUi({log,requestState,svgSrc,documentManager,focusMo
 				height:'100%',
 			}    			
 			//return React.createElement('input',{key:"1",style:inputStyle,onChange:this.onChange,onBlur:this.onBlur,value:this.props.value},null);
-			return React.createElement("div",{style:contStyle},[
-				React.createElement('input',{key:"1",style:inputStyle,onChange:this.onChange,onBlur:this.onBlur,value:this.props.value},null)/*,
-				React.createElement("div",{key:"2",style:popupStyle},[
-					React.createElement("canvas",{ref:ref=>this.canvasEl=ref},null)
-				])*/
-			]);
+			return $("input",{type:"color",key:1,style:inputStyle,onChange:this.onChange, onInput:this.onChange,value:this.props.value, ref:ref=>this.el=ref})
 		}
 	}
 	class ColorPicker extends StatefulComponent{		
@@ -3616,11 +3490,11 @@ ACAAIAAgACAAAA==`
 			}, 1000)
 		}		
 		render(){			
-			return $("div",{},[		    				
+			return [		    				
 				$("div",{key:"mm",ref:ref=>this.mmRef = ref,style:{height:"1mm",position:"absolute",zIndex:"-1"}}),
 				$("div",{key:"em",ref:ref=>this.remRef = ref,style:{height:"1em",position:"absolute",zIndex:"-1"}}),
 				$("div",{key:"children"},this.props.show?this.props.children:null)			
-			])
+			]
 		}
 	}
 	class CanvasMaxHeightElement extends StatefulComponent{
