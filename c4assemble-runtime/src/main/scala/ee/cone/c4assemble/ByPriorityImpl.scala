8d70d12358f52baf41bd2255bc89d195c25b566d
IlyaHalsky <ilya.kachalsky@gmail.com>
2018-09-06 13:18:09 +0300

    Key not found now prints which assemble and join is missing it

--- c4assemble-runtime/src/main/scala/ee/cone/c4assemble/ByPriorityImpl.scala
+++ c4assemble-runtime/src/main/scala/ee/cone/c4assemble/ByPriorityImpl.scala
@@ -1,6 +1,7 @@
 package ee.cone.c4assemble
 
 import scala.collection.immutable.Map
+import scala.util.Try
 
 object ByPriorityImpl extends ByPriority {
   def byPriority[K,V](uses: K⇒(List[K],List[V]⇒V)): List[K] ⇒ List[V] =
@@ -13,7 +14,16 @@ class ByPriorityBuilder[K,V](uses: K⇒(List[K],List[V]⇒V)) {
   private def add(state: PriorityState[K,V], key: K): PriorityState[K,V] =
     if(state.map.contains(key)) state else {
       if (state.inProcess(key)) throw new Exception(s"${state.inProcess.toList.mkString("\n")} \nhas $key")
-      val(useKeys,toValue) = uses(key)
+      val tryGetKey = Try(uses(key))
+      val(useKeys,toValue) =
+        if (tryGetKey.isSuccess)
+          tryGetKey.get
+        else
+          key match {
+            case a: DataDependencyFrom[_] ⇒ throw new Exception(s"JoinKey not found: ${tryGetKey.failed.get.getMessage}\nin assemble ${a.assembleName} in join ${a.name}")
+            case _ ⇒ tryGetKey.get
+          }
+
       val filled = (state.copy(inProcess = state.inProcess + key) /: useKeys)(add)
       val value = toValue(useKeys.map(filled.map))
       state.copy(map = filled.map + (key→value), values = value :: filled.values)
