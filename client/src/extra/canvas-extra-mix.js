b76418720b942dfd7b2f5fbf05e016962b955b03
Sergei Kirjanov <rewlad@gmail.com>
2018-09-14 14:38:30 +0300

    js fix

--- client/src/extra/canvas-extra-mix.js
+++ client/src/extra/canvas-extra-mix.js
@@ -1,7 +1,7 @@
 
 import * as Canvas from "../main/canvas"
 import * as CanvasExtra from "../extra/canvas-extra"
-import {MultiTouchSetup} from "../extra/canvas-multitouch"
+import MultiTouchSetup from "../extra/canvas-multitouch"
 
 export default function CanvasExtraMix(log){ // Canvas.ExchangeCanvasSetup(canvas,feedback,()=>document.body)
     return options => canvas => [
