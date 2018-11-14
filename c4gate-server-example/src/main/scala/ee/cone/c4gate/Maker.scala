2cb039d41cdf7c959a3c4444c9a394d13529e508
Sergei Kirjanov <rewlad@gmail.com>
2018-11-14 19:16:09 +0200

    simple maker/pusher

--- c4gate-server-example/src/main/scala/ee/cone/c4gate/Maker.scala
+++ c4gate-server-example/src/main/scala/ee/cone/c4gate/Maker.scala
@@ -2,6 +2,8 @@ package ee.cone.c4gate
 
 import ee.cone.c4actor._
 
+// C4MAX_REQUEST_SIZE=30000000 C4INBOX_TOPIC_PREFIX='' C4BOOTSTRAP_SERVERS=localhost:8092 C4STATE_TOPIC_PREFIX=ee.cone.c4gate.SimpleMakerApp sbt 'c4gate-server-example/run-main ee.cone.c4actor.ServerMain'
+
 class SimpleMakerApp extends RichDataApp with ExecutableApp
   with EnvConfigApp with VMExecutionApp
   with SnapshotMakingApp with NoAssembleProfilerApp with KafkaConsumerApp
@@ -14,6 +16,25 @@ class SimpleMakerApp extends RichDataApp with ExecutableApp
 class SimpleMakerExecutable(execution: Execution, snapshotMaker: SnapshotMaker) extends Executable {
   def run(): Unit = {
     val Seq(rawSnapshot) = snapshotMaker.make(NextSnapshotTask(None))()
+    execution.complete()
+  }
+}
+
+class SimplePusherApp extends ExecutableApp with EnvConfigApp with VMExecutionApp with NoAssembleProfilerApp with KafkaProducerApp {
+  private lazy val dbDir = "db4"
+  private lazy val rawSnapshotLoader: RawSnapshotLoader = new FileRawSnapshotLoader(dbDir)
+  private lazy val snapshotLoader: SnapshotLoader = new SnapshotLoaderImpl(rawSnapshotLoader)
+  override def toStart: List[Executable] = new SimplePusherExecutable(execution,snapshotLoader,rawQSender) :: super.toStart
+}
+
+class SimplePusherExecutable(execution: Execution, snapshotLoader: SnapshotLoader, rawQSender: RawQSender) extends Executable {
+  def run(): Unit = {
+    val Seq(snapshotInfo) = snapshotLoader.list
+    val Some(event) = snapshotLoader.load(snapshotInfo.raw)
+    rawQSender.send(List(new QRecord {
+      def topic: TopicName = InboxTopicName()
+      def value: Array[Byte] = event.data.toByteArray
+    }))
     execution.complete()
   }
 }
