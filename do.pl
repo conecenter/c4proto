49be061bbb8dd90bab092fdaf3cf7f8b77e88499
IlyaHalsky <eiluu1mooKai>
2019-01-25 12:35:04 +0300

    shhh

--- do.pl
+++ do.pl
@@ -154,7 +154,6 @@ push @tasks, ["stage", sub{
     &$client(1);
 }];
 
-my $dbconf = "DBURL='jdbc:oracle:thin:\@x.edss.ee:64820:mct1' DBUSER='K\$JMS' DBPASS='nQ6Wjspx8nKY7ZKu'";
 my $env = "C4BOOTSTRAP_SERVERS=$bootstrap_server C4INBOX_TOPIC_PREFIX='$inbox_prefix' C4MAX_REQUEST_SIZE=25000000 C4HTTP_SERVER=http://$http_server C4AUTH_KEY_FILE=db4/simple.auth C4HTTP_PORT=$http_port C4SSE_PORT=$sse_port ";
 sub staged{
     $ENV{C4NOSTAGE}?
