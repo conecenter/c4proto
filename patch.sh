cb14fa87408f10d845526fbf1ec1216a21a204ad
Sergei Kirjanov <rewlad@gmail.com>
2019-02-22 00:31:09 +0200

    0.E.9 -- generator single src, vdom optimizations, redraw branch message, jfr, spice, visualvm, FromDB Delete, async current time ticks, txTr skipping

--- patch.sh
+++
@@ -1,3 +0,0 @@
-
-find | grep \\.scala\$ | xargs perl -i -pe 's/\bextends\s+Protocol\s*\{/{/; s/\bAssemble\s+with\b//;  s/\bextends\s+Assemble\s+\{/{/'
-#perl -e 'for(map{[$_=>scalar `cat $_`]}map{/(.+\.scala)\s*$/?"$1":()}`find`){$$_[1]=~/\bAssemble\s*\n\s*with\b/s and print "$$_[0]\n"}'
