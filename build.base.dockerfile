9e361b8f76dd1f07fec225355b1b73fa02bdfeda
Sergei Kirjanov <rewlad@gmail.com>
2019-04-03 12:23:15 +0300

    ci draft

--- build.base.dockerfile
+++ build.base.dockerfile
@@ -5,11 +5,9 @@ RUN perl install.pl apt curl unzip
 RUN perl install.pl curl https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz
 RUN perl install.pl curl https://piccolo.link/sbt-1.2.8.tgz
 RUN perl install.pl apt libyaml-libyaml-perl
-COPY . /c4/c4proto
-RUN mkdir /c4res \
- && chown c4:c4 /c4res \
- && chown -R c4:c4 /c4
-USER c4
 ENV PATH=${PATH}:/tools/jdk/bin:/tools/sbt/bin
+USER c4
+COPY --chown=c4:c4 . /c4/c4proto
+RUN cd /c4/c4proto && ./app.pl build_some_server
 ARG C4CI_FULL_IMG
-RUN perl /c4/c4proto/prod.pl build_some_server ${C4CI_FULL_IMG} /c4/c4proto
+RUN perl /c4/c4proto/prod.pl ci_cp_proto ${C4CI_FULL_IMG} /c4/c4proto
--- prod.pl
+++ prod.pl
@@ -714,12 +714,11 @@ push @tasks, ["compose_up","<tag> $composes_txt",sub{
 #    sy(&$ssh_ctl($build_comp,"-t","docker push $tag"));
 #}];
 
-push @tasks, ["build_some_server","",sub{
+push @tasks, ["ci_cp_proto","",sub{
     my($full_img,$gen_dir)=@_;
     $full_img=~m{^cone/c4zoo:def\.} || die "bad repo/img: $full_img";
-    sy("cd $gen_dir && ./app.pl build_some_server");
-    my $ctx_dir = "/c4res";
-    sy("rm -r $ctx_dir/*");
+    my $ctx_dir = "/c4/res";
+    -e $ctx_dir and sy("rm -r $ctx_dir");
     &$put_text("$ctx_dir/.dockerignore",".dockerignore\nDockerfile");
     sy("cp $gen_dir/zoo.dockerfile $ctx_dir/Dockerfile");
     sy("cp $gen_dir/run.pl $ctx_dir/run.pl");
