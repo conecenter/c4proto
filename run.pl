340b33661e19fabf6183a21b0acaa8c26f2ee376
Sergei Kirjanov <rewlad@gmail.com>
2019-05-17 18:16:26 +0300

    ci draft

--- run.pl
+++ run.pl
@@ -34,7 +34,8 @@ push @tasks, [broker=>sub{
         "zookeeper.connect=$zoo_host:$zoo_port",
         "message.max.bytes=250000000", #seems to be compressed
         "listeners=SSL://:$port", #0.0.0.0
-        "advertised.listeners=SSL://127.0.0.1:$port,SSL://$add",
+        "advertised.listeners=INTERNAL://127.0.0.1:$port,EXTERNAL://$add",
+        "listener.security.protocol.map=INTERNAL:SSL,EXTERNAL:SSL",
     );
     my $props = $ENV{C4SSL_PROPS} || die;
     sy("cat $props >> server.properties");
