b84d6aad53a5f930b2f53fdd590374bab12c80df
IlyaHalsky <ik@cone.ee>
2019-09-11 11:22:42 +0300

    mime type fix

--- c4gate-publish/src/main/scala/ee/cone/c4gate/Publish.scala
+++ c4gate-publish/src/main/scala/ee/cone/c4gate/Publish.scala
@@ -38,12 +38,12 @@ class PublishingObserver(
   }
   def publish(global: RichContext)(path: String, body: Array[Byte]): Unit = {
     val pointPos = path.lastIndexOf(".")
-    val ext = if(pointPos<0) None else Option(path.substring(pointPos+1))
+    val ext = if(pointPos<0) "" else path.substring(pointPos+1)
     val byteString = compressor.compress(ToByteString(body))
     val headers =
       N_Header("ETag", s""""${idGenUtil.srcIdFromSerialized(0,byteString)}"""") ::
       N_Header("Content-Encoding", compressor.name) ::
-      ext.flatMap(mimeTypes).map(N_Header("Content-Type",_)).toList
+        mimeTypes(ext).map(N_Header("Content-Type",_)).toList
     val publication = S_HttpPublication(path,headers,byteString,None)
     val existingPublications = ByPK(classOf[S_HttpPublication]).of(global)
     //println(s"${existingPublications.getOrElse(path,Nil).size}")
