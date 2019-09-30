65c1fdb52d4779d68ee31430df34289e24d8b311
IlyaHalsky <ik@cone.ee>
2019-09-30 17:33:47 +0300

    akka reconfigure

--- c4gate-client/src/main/scala/ee/cone/c4gate/PrometheusMix.scala
+++ c4gate-client/src/main/scala/ee/cone/c4gate/PrometheusMix.scala
@@ -8,7 +8,7 @@ import scala.util.Try
 trait PrometheusPostApp extends AssemblesApp with DefaultMetricsApp {
   def config: Config
 
-  lazy val defaultPrometheusPostRefresh: Long = 60L * 1000L
+  lazy val defaultPrometheusPostRefresh: Long = 30L * 1000L
   lazy val defaultPrometheusPostUrl: Option[String] = Try(config.get("C4PROMETHEUS_POST_URL")).toOption
 
   override def assembles: List[Assemble] = new PrometheusPostAssemble(metricFactories, defaultPrometheusPostUrl.map(url ⇒ PrometheusPostSettings(url, defaultPrometheusPostRefresh))) :: super.assembles
