e883a2ff6a4355ff0b4fb316efd8155ac8554b2f
Sergei Kirjanov <rewlad@gmail.com>
2019-11-26 17:43:20 +0200

    clean up

--- base_lib/src/main/scala/ee/cone/c4proto/package.scala
+++ base_lib/src/main/scala/ee/cone/c4proto/package.scala
@@ -8,10 +8,4 @@ package object c4proto {
   class protocol(apps: String*) extends StaticAnnotation
 
   @deprecated type Protocol = AbstractComponents
-  @deprecated type Component = ee.cone.c4di.Component
-  @deprecated type c4 = ee.cone.c4di.c4
-  @deprecated type provide = ee.cone.c4di.provide
-  //@deprecated type TypeKey = ee.cone.c4di.TypeKey
-  //@deprecated def TypeKey = ee.cone.c4di.TypeKey
-  @deprecated type AutoMixer = ee.cone.c4di.AutoMixer
 }
