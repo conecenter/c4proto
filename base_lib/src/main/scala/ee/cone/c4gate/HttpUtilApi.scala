1eafa78a5a8ba854aef103a5dda7ce839e76d06f
Aleksandr Senchenko <awb.senchenko@gmail.com>
2019-12-02 11:34:50 +0300

    + HttpUtil post

--- base_lib/src/main/scala/ee/cone/c4gate/HttpUtilApi.scala
+++ base_lib/src/main/scala/ee/cone/c4gate/HttpUtilApi.scala
@@ -8,4 +8,20 @@ trait HttpUtil {
   def get(url: String, headers: List[(String, String)]): HttpResponse
   def post(url: String, headers: List[(String, String)]): Unit
   def post(url: String, headers: List[(String, String)], body: ByteString, timeOut: Option[Int], expectCode: Int): Unit
+  def put(url: String, headers: List[(String, String)], body: ByteString, timeOut: Option[Int]): Int
+  def put(url: String, headers: List[(String, String)], body: ByteString): Int
+}
+object HttpUtil {
+  sealed trait HttpMethod
+  object HttpMethod {
+    case object PUT extends HttpMethod
+    case object POST extends HttpMethod
+    case object GET extends HttpMethod
+    case object OPTIONS extends HttpMethod
+    case object HEAD extends HttpMethod
+    case object PATCH extends HttpMethod
+    case object DELETE extends HttpMethod
+    case object TRACE extends HttpMethod
+    case object CONNECT extends HttpMethod
+  }
 }
