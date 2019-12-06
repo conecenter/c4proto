c93a901a8a3ca78f499c160a4c9fa9ba4b970628
Sergei Kirjanov <rewlad@gmail.com>
2019-12-06 22:33:20 +0200

    ready/waiting observer

--- base_lib/src/main/scala/ee/cone/c4actor/ProgressObserverImpl.scala
+++ base_lib/src/main/scala/ee/cone/c4actor/ProgressObserverImpl.scala
@@ -3,19 +3,35 @@ package ee.cone.c4actor
 import java.nio.charset.StandardCharsets.UTF_8
 import java.nio.file.{Files, Path, Paths}
 import java.time.Instant
+import java.util.UUID
 
 import com.typesafe.scalalogging.LazyLogging
 import ee.cone.c4actor.QProtocolBase.S_Firstborn
 import ee.cone.c4actor.Types.{NextOffset, SrcId}
 import ee.cone.c4assemble.Types.{Each, Values}
-import ee.cone.c4assemble.c4assemble
+import ee.cone.c4assemble.{Single, c4assemble}
 import ee.cone.c4di.c4
 
 import scala.concurrent.Future
 
-@c4("ServerCompApp") class ProgressObserverFactoryImpl(inner: TxObserver) extends ProgressObserverFactory {
-  def create(endOffset: NextOffset): Observer[RichContext] = new ProgressObserverImpl(inner.value,endOffset)
+@c4("ServerCompApp") class ProgressObserverFactoryImpl(inner: TxObserver, config: ListConfig) extends ProgressObserverFactory {
+  def create(endOffset: NextOffset): Observer[RichContext] = {
+    val readyObserver = Single.option(config.get("C4ROLLING")).fold(inner.value)(path=>
+      new ReadyObserverImpl(inner.value, Paths.get(path), 0L)
+    )
+    new ProgressObserverImpl(readyObserver,endOffset)
+  }
 }
+
+// states:
+//   loading
+//   loading ready
+//   master
+// trans:
+//   loading -> loading
+//   loading -> loading ready
+//   loading ready -> loading ready
+//   loading ready -> master
 
 class ProgressObserverImpl(inner: Observer[RichContext], endOffset: NextOffset, until: Long=0) extends Observer[RichContext] with LazyLogging {
   def activate(rawWorld: RichContext): Observer[RichContext] =
@@ -30,6 +46,22 @@ class ProgressObserverImpl(inner: Observer[RichContext], endOffset: NextOffset, 
       inner.activate(rawWorld)
     }
 }
+
+class ReadyObserverImpl(inner: Observer[RichContext], path: Path, until: Long=0) extends Observer[RichContext] with LazyLogging {
+  def activate(rawWorld: RichContext): Observer[RichContext] = {
+    if(until == 0) Files.write(path.resolve("c4is-ready"),Array.empty[Byte])
+    val now = System.currentTimeMillis
+    if(now < until) this
+    else if(Files.exists(path.resolve("c4is-master"))) {
+      logger.info(s"becoming master")
+      inner.activate(rawWorld)
+    } else {
+      logger.debug(s"ready/waiting")
+      new ReadyObserverImpl(inner, path, now+1000)
+    }
+  }
+}
+
 
 @c4assemble("ServerCompApp") class BuildVerAssembleBase(config: ListConfig, execution: Execution){
   def join(
