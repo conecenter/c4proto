fb35d608d451ef8e813625fcf0dd1cdb2ba73093
IlyaHalsky <ik@cone.ee>
2020-02-27 12:01:48 +0300

    time getters

--- extra_examples/src/main/scala/ee/cone/c4actor/tests/TimeGenTest.scala
+++ extra_examples/src/main/scala/ee/cone/c4actor/tests/TimeGenTest.scala
@@ -1,11 +1,12 @@
 package ee.cone.c4actor.tests
 
 import ee.cone.c4actor.QProtocol.S_Firstborn
-import ee.cone.c4actor.TxTransform
+import ee.cone.c4actor.{AssembledContext, TxTransform}
 import ee.cone.c4actor.Types.SrcId
 import ee.cone.c4actor.time._
 import ee.cone.c4assemble.Types.{Each, Values}
 import ee.cone.c4assemble.c4assemble
+import ee.cone.c4di.c4
 
 @c4time(0x1337) case object TestTestTime extends CurrentTime(10L)
 
@@ -15,4 +16,8 @@ import ee.cone.c4assemble.c4assemble
     fb: Each[S_Firstborn],
     @time(TestTestTime) time: Each[T_Time]
   ): Values[(SrcId, TxTransform)] = Nil
+}
+
+@c4 class TestTime(timeGetters: TimeGetters) {
+  val test: AssembledContext => Option[T_Time] = timeGetters(TestTestTime).ofA
 }
