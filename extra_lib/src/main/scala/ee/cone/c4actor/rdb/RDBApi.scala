446ed28171492576ee30312449084d8b28a882d6
Igor <it@cone.ee>
2020-03-03 15:32:42 +0200

    dbOptions fix

--- extra_lib/src/main/scala/ee/cone/c4actor/rdb/RDBApi.scala
+++ extra_lib/src/main/scala/ee/cone/c4actor/rdb/RDBApi.scala
@@ -4,11 +4,15 @@ package ee.cone.c4actor.rdb
 import ee.cone.c4assemble.Assemble
 
 trait ExternalDBOption
+trait CommonDBOption extends ExternalDBOption{
+  def cl:Class[_]
+  lazy val className: String = cl.getName
+}
 
 trait RDBOptionFactory {
   def fromDB[P<:Product](cl: Class[P]): ExternalDBOption
   def toDB[P<:Product](cl: Class[P], code: List[String]): ExternalDBOption
 }
 
-class ToDBOption(val className: String, val code: List[String], val assemble: Assemble) extends ExternalDBOption
-class FromDBOption(val className: String) extends ExternalDBOption
+class ToDBOption(val cl:Class[_], val code: List[String], val assemble: Assemble) extends CommonDBOption
+class FromDBOption(val cl:Class[_]) extends CommonDBOption
