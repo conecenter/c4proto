8046a43a51a8325c1984cf88160306c72ab91737
Dmitry Lipodat <dl@cone.ee>
2020-03-31 14:49:46 +0300

    DynamicIndexModelsApp

--- extra_lib/src/main/scala/ee/cone/c4actor/hashsearch/base/HashSearchAssemble.scala
+++ extra_lib/src/main/scala/ee/cone/c4actor/hashsearch/base/HashSearchAssemble.scala
@@ -3,7 +3,7 @@ package ee.cone.c4actor.hashsearch.base
 import ee.cone.c4actor.HashSearch.{Request, Response}
 import ee.cone.c4actor._
 import ee.cone.c4actor.Types.SrcId
-import ee.cone.c4actor.hashsearch.index.dynamic.{DynamicIndexModelsApp, DynamicIndexModelsProvider}
+import ee.cone.c4actor.hashsearch.index.dynamic.DynamicIndexModelsProvider
 import ee.cone.c4assemble.Types.{Each, Values}
 import ee.cone.c4assemble._
 import ee.cone.c4di.{c4, provide}
@@ -59,9 +59,7 @@ trait HashSearchAssembleSharedKeys {
   type SharedResponseId = SrcId
 }
 
-trait HashSearchModelsApp extends DynamicIndexModelsApp
-
-trait HashSearchAssembleAppBase extends HashSearchModelsApp
+trait HashSearchAssembleAppBase
 
 class HashSearchAssembleDebug(val value: Boolean)
 
