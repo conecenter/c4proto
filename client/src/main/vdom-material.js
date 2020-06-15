c53b75fc8117af36d50a12bad923044b451edb9b
Sergei Kirjanov <rewlad@gmail.com>
2020-06-15 16:46:51 +0300

    material menu draft

---
+++ client/src/main/vdom-material.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+export const materialTransforms = ({tp:{Button,Menu,MenuItem}})
+
+/*
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+
+        onClose={handleClose}
+
+
+        anchorReference="anchorPosition"
+        anchorPosition={
+          state.mouseY !== null && state.mouseX !== null
+            ? { top: state.mouseY, left: state.mouseX }
+            : u
+
+*/
