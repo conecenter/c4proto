c61768e07268da36e2efda0621e04faa4e6a6075
Sergei Kirjanov <rewlad@gmail.com>
2020-08-28 17:46:24 +0300

    tag-gen draft

---
+++ base_examples/src/main/scala/ee/cone/c4ui/AutoTags.scala
@@ -0,0 +1,164 @@
+package ee.cone.c4ui
+
+import ee.cone.c4di._
+import ee.cone.c4vdom.{ChildPairFactory,MutableJsonBuilder,ToJson}
+
+class c4tags(a: String*)
+
+trait VDomItem extends Product
+trait LReceiver
+
+trait ImagePublicPath
+trait Seed
+object SVGPublicPath {
+  def empty: ImagePublicPath = ???
+}
+trait FieldValue
+trait MetaAttrList
+trait CanEl
+
+trait JsonAdapter[T] {
+  def appendJson(key: String, value: T, builder: MutableJsonBuilder): Unit
+  def appendJson(value: T, builder: MutableJsonBuilder): Unit
+}
+
+// ========================== //
+
+trait FlexItemData extends Product
+case class FlexItem(key: String, attr:FlexItemData, children:List[FlexItem]) extends VDomItem
+trait InlineItem extends VDomItem
+
+case class FColData(layoutParams: Option[LayoutParams], onClick: Option[LReceiver]) extends FlexItemData
+
+trait Sticky
+case object NonSticky extends Sticky
+case object OneWaySticky extends Sticky //hides on scroll down, shows on scroll up, like menu
+case object FullSticky extends Sticky //never hides full width row, like error
+
+trait LayoutParams {
+  def widthBasis: Option[BigDecimal]
+  def widthMax: Option[BigDecimal]
+  def parentWidth: Boolean
+  def align: Unit
+  def sticky: Sticky
+}
+
+object DefaultLayout extends LayoutParams {
+  def widthBasis: Option[BigDecimal] = None
+  def widthMax: Option[BigDecimal] = None
+  def parentWidth: Boolean = false
+  def align: Unit = ???  // left
+  def sticky: Sticky = NonSticky
+}
+object DefaultPopupLayout extends LayoutParams {
+  def widthBasis: Option[BigDecimal] = None
+  def widthMax: Option[BigDecimal] = None // + limit % of ViewPort in css class (~90%)
+  def parentWidth: Boolean = false
+  def align: Unit = ???  // right in relation of inducing element
+  def sticky: Sticky = NonSticky
+}
+
+object NoReceiver extends LReceiver {
+  //def receive: NoReceiver.Handler = ??? // never
+}
+
+trait Color // text + background
+trait PaletteColor extends Color // from interface palette, fixed pairs
+trait CustomColor extends Color // background by user data (not code), text by contrast formula
+
+trait GroupBoxStyle // only 2 for this moment
+object DefaultGroupBoxStyle extends GroupBoxStyle
+object StandOutGroupBoxStyle extends GroupBoxStyle
+
+// union-types emulated by contra-variance
+trait VDom[-C]
+
+trait OfFlex extends VFlexCol with VFlexRow with VRow with VMenu with VFilterExtra1 with VFilterExtra2 with VFlexCell with VIFrame with VCanvas with VAccordion
+trait OfFieldFrame extends OfFlex with VField
+trait OfRoot extends OfFlex with VHeader
+trait InLines extends VText with VLabel with VImage with VInput with VButton with VChip
+trait OfTablePart extends VTr
+trait OfTr extends VTh with VTd
+
+trait VElement
+trait VRoot extends VElement
+trait VFlexCol extends VElement
+trait VFlexRow extends VElement
+sealed trait VRow extends VElement
+trait VMenu extends VElement
+sealed trait VFilterExtra1 extends VElement
+sealed trait VFilterExtra2 extends VElement
+trait VTable extends VElement
+trait VTr extends VElement
+trait VTh extends VElement
+trait VTd extends VElement
+trait VFlexCell extends VElement
+trait VText extends VElement
+trait VLabel extends VElement
+trait VImage extends VElement
+sealed trait VInput extends VElement
+trait VButton extends VElement
+trait VChip extends VElement
+trait VField extends VElement
+trait VHeader extends VElement
+trait VIFrame extends VElement
+trait VCanvas extends VElement
+trait VAccordion extends VElement
+
+trait VTitle extends VElement
+trait VAccordionItem extends VElement
+
+
+@c4tags("AAA") trait AutoTags {
+  // onclick = cursor hand + mouse over
+  // no font size <- css font size + special mode "zoom all to fit"
+  def flexCol(key: String, layoutParams: LayoutParams = DefaultLayout, onClick: LReceiver = NoReceiver)(children: VDom[OfFlex]*): VDom[VFlexCol]
+  def flexRow(key: String, layoutParams: LayoutParams = DefaultLayout, onClick: LReceiver = NoReceiver)(children: VDom[OfFlex]*): VDom[VFlexRow]
+  def table(key: String, layoutParams: LayoutParams = DefaultLayout, onClick: LReceiver = NoReceiver, stickyColumns: Int = 0)(headers: VDom[OfTablePart]*)(rows: VDom[OfTablePart]*): VDom[VTable] // headers allways sticky if rows > 1
+  def tr(key: String, onClick: LReceiver = NoReceiver)(children: VDom[OfTr]*): VDom[VTr]
+  def td(key: String, colSpan: Int = 1, rowSpan: Int = 1, layoutParams: LayoutParams = DefaultLayout, onClick: LReceiver = NoReceiver)(children: VDom[OfFieldFrame]*): VDom[VTd]
+
+  // def setCurrent[T<:VElement](current: Boolean)(el: VDom[T]): VDom[T]
+
+  def flexCell(key: String, layoutParams: LayoutParams = DefaultLayout, onClick: LReceiver = NoReceiver)(children: VDom[InLines]*): VDom[VFlexCell]
+  // def flexCell(layoutParams: LayoutParams = DefaultLayout)(child: VDom[InLines]): VDom[VFlexCell] // key from child
+  def text(key: String, text: String, onClick: LReceiver): VDom[VText]
+  def label(key: String, text: String, onClick: LReceiver): VDom[VLabel]
+  def image(key: String, path: ImagePublicPath, onClick: LReceiver = NoReceiver): VDom[VImage] // IT todo: + rotate in ImagePublicPath
+  def button(key: String, color: PaletteColor, onClick: LReceiver = NoReceiver)(children: VDom[InLines]*): VDom[VButton] // NoReceiver == css class for disabled button
+  def chip(key: String, color: Color, onClick: LReceiver = NoReceiver)(children: VDom[InLines]*): VDom[VChip]
+
+  def title(key: String = "single", onClick: LReceiver = NoReceiver)(children: VDom[InLines]*): VDom[VTitle] // no key <- allways single child
+  def noTitle(key: String = "single"): VDom[VTitle] // implimentation: val noTitle: VDom[VTitle] = title()()
+
+  def root(key: String = "single")(children: VDom[OfRoot]*): VDom[VRoot]
+  def rootHeader(key: String, onClick: LReceiver = NoReceiver)(children: VDom[OfFlex]*): VDom[VHeader]
+  def iframe(key: String, layoutParams: LayoutParams = DefaultLayout, seed: Seed): VDom[VIFrame]
+
+  //def virtualKeyboard(???)
+
+  def groupBox(key: String, title: VDom[VTitle] = noTitle(), groupBoxStyle: GroupBoxStyle = DefaultGroupBoxStyle, layoutParams: LayoutParams = DefaultLayout, onClick: LReceiver = NoReceiver)(children: VDom[OfFlex]*): VDom[VFlexCol]
+  // SK todo popup: own subtree, like seeds, stack of popup case classes, auto close by focus
+  def tabsHolder(key: String, layoutParams: LayoutParams = DefaultLayout, onClick: LReceiver = NoReceiver)(children: VDom[OfFlex]*): VDom[VFlexRow] // high level tabs?
+  def canvas(key: String, layoutParams: LayoutParams = DefaultLayout, onClick: LReceiver = NoReceiver)(children: VDom[CanEl]*): VDom[VCanvas] // sk todo: ???
+
+  def filter(key: String = "single")(filters: VDom[OfFlex]*)(selectedRowsButtons: VDom[InLines]*)(allRowsButtons: VDom[InLines]*): VDom[VRow] // or VDom[VFlexRow] but without wrap?
+
+  def menu(key: String, layoutParams: LayoutParams = DefaultLayout, onClick: LReceiver = NoReceiver)(menuItems: VDom[OfFlex]*): VDom[VMenu]
+  def menuItem(key: String, path: ImagePublicPath = SVGPublicPath.empty, onClick: LReceiver = NoReceiver)(children: VDom[InLines]*): VDom[VFlexCell] // popup side?
+
+  def accordion(key: String, columns: Int = 2)(items: VDom[VAccordionItem]*): VDom[VAccordion]
+  def accordionItem(key: String, opened: Boolean, mainTitle: VDom[VTitle], rightTitle: VDom[VTitle] = noTitle(), columnsWide: Int = 1)(children: VDom[OfFlex]*): VDom[VAccordionItem] // callback? or children only when open?
+
+  def fieldFrame(
+    key: String, title: VDom[VTitle], // title mandatory, vertical space for title is always reserved as 1 line
+    layoutParams: LayoutParams = DefaultLayout, events: LReceiver = NoReceiver
+  )(inputLineChildren: VDom[OfFieldFrame]*): VDom[VFlexCol] // onclick + focus???
+
+  def field( // force no use VK on iPad by route
+    key: String,
+    value: FieldValue, valueMetas: MetaAttrList = Nil,
+    layoutParams: LayoutParams = DefaultLayout, events: LReceiver = NoReceiver
+  )(addValue: VDom[InlineItem]*): VDom[VField]
+}
+
--- generator/src/main/scala/ee/cone/c4generator/TagGenerator.scala
+++ generator/src/main/scala/ee/cone/c4generator/TagGenerator.scala
@@ -2,36 +2,122 @@ package ee.cone.c4generator
 
 import scala.meta._
 
+case class TagParam(paramName: String, paramTypeName: String)
+
 object TagGenerator extends Generator {
-  case class Param(paramName: String, paramTypeName: String)
-
-
   def get(parseContext: ParseContext): List[Generated] = parseContext.stats.flatMap{
-    case Defn.Trait(Seq(mod"@c4tags"),Type.Name(traitName),x,y,code) =>
-      code.stats.flatMap{
+    case Defn.Trait(Seq(mod"@c4tags(...$e)"),Type.Name(traitName),x,y,code) =>
+      val mod = mod"@c4(...$e)".syntax
+      val modMulti = mod"@c4multi(...$e)".syntax
+      val res: List[TagStatements] = code.stats.map{
         case defDef@q"def $defName(...$args): VDom[${Type.Name(outTypeName)}]" =>
-
           val argByIsChild = args.flatten.map{
-            case p@param"${Name(paramName)}: VDom[${Type.Name(paramTypeName)}]*" =>
-              (true,Param(paramName, paramTypeName))
-            case p@param"${Name(paramName)}: ${Type.Name(paramTypeName)} = $paramExprOpt" =>
-              (false,Param(paramName, paramTypeName))
+            case p@Term.Param(Nil,Term.Name(paramName),Some(paramType),_) =>
+              paramType match {
+                case t"VDom[${Type.Name(paramTypeName)}]" =>
+                  (true,TagParam(paramName, paramTypeName))
+                case t"VDom[${Type.Name(paramTypeName)}]*" =>
+                  (true,TagParam(paramName, paramTypeName))
+                case Type.Name(paramTypeName) =>
+                  (false,TagParam(paramName, paramTypeName))
+                case p =>
+                  throw new Exception(s"unsupported tag param type [$p] ${p.structure} of $defName")
+              }
           }.groupMap(_._1)(_._2)
-          val childArgs = argByIsChild.getOrElse(true,Nil)
-          val attrArgs = argByIsChild.getOrElse(false,Nil)
-
-
-
-          ???
+          val childArgs = argByIsChild.getOrElse(true,Nil).toList
+          val attrArgs = argByIsChild.getOrElse(false,Nil).toList match {
+            case TagParam("key","String") :: attrArgs => attrArgs
+            case p => throw new Exception(s"need key param for $defName")
+          }
+          TagStatements(defDef.syntax, defName.value, attrArgs, childArgs, outTypeName, mod, modMulti)
       }
-      ???
+      val tagClasses = res.map(_.getTagClass)
+      MultiGenerator.getForStats(tagClasses.map(_.parse[Stat].get)) ++
+      tagClasses.map(GeneratedCode) ++
+      res.map(_.getAdapterClass).map(GeneratedCode) ++
+      List(GeneratedCode(
+        s"\n$mod final class ${traitName}Impl(" +
+        "\n  child: ChildPairFactory, " +
+        res.map(_.getArg).mkString +
+        s"\n) extends ${traitName} {" +
+        res.map(_.getDef).mkString +
+        s"\n}"
+      ))
     case _ => Nil
   }
+}
 
-
+case class TagStatements(
+  defDef: String, defName: String,
+  attrArgs: List[TagParam], childArgs: List[TagParam], outTypeName: String,
+  mod: String, modMulti: String,
+){
+  def getArg: String = s"\n  ${defName}Factory:  ${outTypeName}TagFactory, "
+  def getDef: String = {
+    val childArgsStr = childArgs.foldRight("Nil")((param,res)=>
+      s"""child.addGroup("${param.paramName}",${param.paramName},$res)"""
+    )
+    val attrArgsStr = attrArgs.map(_.paramName).mkString(",")
+    s"\n  $defDef = child[$outTypeName](key, ${defName}Factory.create($attrArgsStr), $childArgsStr)"
+  }
+  def getTagClass: String =
+    s"\n$modMulti final case class ${outTypeName}Tag(" +
+    attrArgs.map(param=>s"\n  ${param.paramName}: ${param.paramTypeName}, ").mkString +
+    s"\n)(adapter: JsonAdapter[${outTypeName}Tag]) extends ToJson {" +
+    s"\n  def appendJson(builder: MutableJsonBuilder): Unit = adapter.appendJson(this, builder)" +
+    "\n}"
+  def getAdapterClass: String =
+    s"\n$mod final class ${outTypeName}TagJsonAdapter(" +
+    attrArgs.map(param=>s"\n  ${param.paramName}JsonAdapter: JsonAdapter[${param.paramTypeName}], ").mkString +
+    s"\n) extends JsonAdapter[${outTypeName}Tag] {" +
+    s"\n  def appendJson(key: String, value: ${outTypeName}Tag, builder: MutableJsonBuilder): Unit = {" +
+    s"\n    builder.append(key)" +
+    s"\n    appendJson(value,builder)" +
+    s"\n  }" +
+    s"\n  def appendJson(value: ${outTypeName}Tag, builder: MutableJsonBuilder): Unit = {" +
+    s"\n    builder.startObject()" +
+    "\n" + s"""    builder.append("tp").append("$outTypeName")""" +
+    attrArgs.map(param =>
+      "\n" + s"""    ${param.paramName}JsonAdapter.appendJson("${param.paramName}", value.${param.paramName}, builder)"""
+    ).mkString +
+    s"\n    builder.end()" +
+    s"\n  }" +
+    s"\n}"
 }
 
 /*
+
+
+
+
+@c4 final class BbbJsonAdapter(
+  a1JsonAdapter: JsonAdapter[A1],
+  a2JsonAdapter: JsonAdapter[A2],
+) extends JsonAdapter[Bbb] {
+  def appendJson(key: String, value: Bbb, builder: MutableJsonBuilder): Unit = {
+    builder.append(key)
+    appendJson(value,builder)
+  }
+  def appendJson(value: Bbb, builder: MutableJsonBuilder): Unit = {
+    builder.startObject()
+    builder.append("tp").append("Bbb")
+    a1JsonAdapter.appendJson("a1", value.a1, builder)
+    a2JsonAdapter.appendJson("a2", value.a2, builder)
+    builder.end()
+  }
+}
+
+@c4multi final case class Bbb(a1: A1, a2: A2)(adapter: JsonAdapter[Bbb]) extends ToJson {
+  def appendJson(builder: MutableJsonBuilder): Unit = adapter.appendJson(this, builder)
+}
+
+@c4 final class Ddd(
+  child: ChildPairFactory,
+  bbbFactory: BbbFactory,
+) {
+  def bbb(key: String, a1: A1, a2: A2)(c1: VDom[C1]*)(c2: VDom[C2]*): VDom[B] =
+    child[B](key, bbbFactory.create(a1,a2), child.addGroup("c1",c1,child.addGroup("c2",c2,Nil)))
+
 
 Tag
 def
