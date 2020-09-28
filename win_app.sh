af17771c66cc500def51dd95840e372c1b48f08a
Ilya Halsky <ilya.kachalsky@gmail.com>
2020-09-28 21:34:13 +0300

    wsl scripts

--- win_app.sh
+++ win_app.sh
@@ -1,17 +1,25 @@
+set -x
+
 install() {
   sudo apt-get update
   sudo apt-get install gcc -y
   sudo apt-get make -y
-  cp -r /mnt/c/Users/ilya2/.ssh ~/.ssh
+  cp -r /mnt/c/Users/$1/.ssh ~/.ssh
   chmod 600 ~/.ssh/id_rsa
+  chmod 600 ~/.ssh/id_rsa.pub
   curl -L http://xrl.us/installperlnix | bash
 }
 
 build_all() {
   export C4BUILD_PORT=22
   export C4BUILD_ADDR="127.0.21.1"
-  export C4CONN_CMD="ssh.exe"
   ./app.pl build_all
+}
+
+build_diff() {
+  export C4BUILD_PORT=22
+  export C4BUILD_ADDR="127.0.21.1"
+  C4BUILD_CLEAN=0 ./app.pl build_all
 }
 
 while test $# -gt 0; do
@@ -20,10 +28,13 @@ while test $# -gt 0; do
     echo "bad option $1"
     ;;
   install)
-    install
+    install $2
     ;;
   build_all)
     build_all
+    ;;
+  build_diff)
+    build_diff
     ;;
   *)
     echo "unsupported argument $1"
