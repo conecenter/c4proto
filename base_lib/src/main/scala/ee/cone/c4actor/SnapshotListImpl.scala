5054c900a6002d96aa88c6300bbdacfc64fce824
Andrei Mavrin <andreyshambala@gmail.com>
2020-10-09 16:17:50 +0300

    beginning offset

--- base_lib/src/main/scala/ee/cone/c4actor/SnapshotListImpl.scala
+++ base_lib/src/main/scala/ee/cone/c4actor/SnapshotListImpl.scala
@@ -4,5 +4,5 @@ import ee.cone.c4actor.Types.NextOffset
 import ee.cone.c4di.c4
 
 @c4("SnapshotListProtocolApp") final class LastSnapshotGetterImpl(consuming: Consuming) extends LastSnapshotGetter {
-  def get(): NextOffset = consuming.process("0" * OffsetHexSize(), _.endOffset)
+  def get(): NextOffset = consuming.process("0" * OffsetHexSize(), _.beginningOffset)
 }
