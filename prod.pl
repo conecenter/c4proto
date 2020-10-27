ec4377e39f4c22fb5cfe423433c69281dcfd750a
Ilya Kachalsky <ilya.kachalsky@gmail.com>
2020-10-27 14:56:53 +0200

    Fix image build (#327)
    * fix
    
    * fix 2
    
    * fix 3
    
    * fix 4
    
    * fix 5
    
    * fix 6
    
    * fix 7
    
    * fix 8
    
    * fix 9
    
    * fix 10

--- prod.pl
+++ prod.pl
@@ -1070,7 +1070,7 @@ my $up_desktop = sub{
             "RUN perl install.pl curl $dl_frp_url",
             "RUN perl install.pl curl https://nodejs.org/dist/v8.9.1/node-v8.9.1-linux-x64.tar.xz",
             "RUN perl install.pl curl https://github.com/sbt/sbt/releases/download/v1.4.0/sbt-1.4.0.tgz",
-            "RUN perl install.pl curl https://git.io/coursier-cli && chmod +x /tools/coursier",
+            "RUN perl install.pl curl https://git.io/coursier-cli-linux && chmod +x /tools/coursier",
             "RUN rm -r /etc/dropbear && ln -s /c4/dropbear /etc/dropbear ",
             "COPY desktop.pl haproxy.pl bloop_fix.pl id_rsa.pub c4p_alias.sh /",
             "RUN C4DATA_DIR=/c4db perl /desktop.pl fix",
@@ -1418,7 +1418,7 @@ push @tasks, ["ci_inner_build","",sub{
     my ($base,$gen_dir,$proto_dir) = &$ci_inner_opt();
     sy("perl $proto_dir/bloop_fix.pl");
     sy("bloop server &");
-    my $find = sub{ syf("jcmd")=~/^(\d+)\s+\S+\bblp-server\b/ and return "$1" while sleep 1; die };
+    my $find = sub{ syf("jcmd")=~/^(\d+)\s+bloop\.Server\b/ and return "$1" while sleep 1; die };
     my $pid = &$find();
     sy("cd $gen_dir && perl $proto_dir/build.pl");
     my $close = &$start("cd $gen_dir && sh .bloop/c4/tag.$base.compile");
@@ -1500,7 +1500,7 @@ push @tasks, ["ci_inner_cp","",sub{ #to call from Dockerfile
     my @classpath = syf("cat $gen_dir/.bloop/c4/mod.$mod.classpath")=~/([^\s:]+)/g;
     my @started = map{&$start($_)} map{
         m{([^/]+\.jar)$} ? "cp $_ $ctx_dir/app/$1" :
-        m{([^/]+)\.classes$} ? "cd $_ && zip -q -r $ctx_dir/app/$1.jar ." : die
+        m{([^/]+)\.classes(-bloop-cli)?$} ? "cd $_ && zip -q -r $ctx_dir/app/$1.jar ." : die $_
     } @classpath;
     &$_() for @started;
     &$put_text("$ctx_dir/serve.sh","export C4APP_CLASS=$main\nexec java ee.cone.c4actor.ServerMain");
