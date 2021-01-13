36e9e099b0a7c93dd4c88bcc3dc10aabe1165a7c
Sergei Kirjanov <rewlad@gmail.com>
2021-01-13 18:16:01 +0200

    repos

--- repos.pl
+++ repos.pl
@@ -8,12 +8,14 @@ my @dirs = map{ /^(.+).git\s*$/ ? "$1" : die } syl("find -iname .git");
 for my $dir(@dirs){
     print "####################################### $dir\n";
     my $git = "cd $dir && git ";
-    for(syl("$git remote -v")){
-        my($nm,$ln) = m{^(origin)\s+https://github\.com/(\w+/\w+\.git)\s+\(push\)\s*$} ? ($1,$2) : next;
-        print "COPY/RUN:    ($git remote set-url --push $nm git\@github.com:$ln)\n";
-    }
     print syl("$git status");
-    print `$git push origin --all --dry-run`;
+    my @set_msg = map{
+        m{^(origin)\s+https://github\.com/(\w+/\w+\.git)\s+\(push\)\s*$} ? do{
+            my($nm,$ln) = ($1,$2);
+            "COPY/RUN:    ($git remote set-url --push $nm git\@github.com:$ln)\n";
+        } : ();
+    } syl("$git remote -v");
+    if(@set_msg){ print @set_msg } else {print `$git push origin --all --dry-run`}
 }
 
 #### CALL: ssh-agent c4proto/repos.pl
