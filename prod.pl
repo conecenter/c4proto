ccaa0138ba90bbcd83d994b44ff6c625b11e5e35
Sergei Kirjanov <rewlad@gmail.com>
2021-01-22 22:40:06 +0200

    s3 consumer draft

--- prod.pl
+++ prod.pl
@@ -4,7 +4,7 @@ use strict;
 use Digest::MD5 qw(md5_hex);
 use JSON::XS;
 
-my $sys_image_ver = "v80h";
+my $sys_image_ver = "v80i";
 
 sub so{ print join(" ",@_),"\n"; system @_; }
 sub sy{ print join(" ",@_),"\n"; system @_ and die $?; }
