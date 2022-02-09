f71af2520637964bd51f5893c19abe51da14d59d
Sergei Kirjanov <rewlad@gmail.com>
2022-02-09 16:44:29 +0200

    fixed npm changes package.json

--- prod.pl
+++ prod.pl
@@ -1476,7 +1476,7 @@ my $build_client = sub{
     unlink or die $! for <$build_dir/*>;
     my $conf_dir = &$single_or_undef(grep{-e} map{"$_/webpack"} <$dir/src/*>) || die;
     &$if_changed("$dir/package.json", syf("cat $conf_dir/package.json"), sub{1})
-        and sy("cd $dir && npm install");
+        and sy("cd $dir && npm install --no-save");
     sy("cd $dir && cp $conf_dir/webpack.config.js . && cp $conf_dir/tsconfig.json . && node_modules/webpack/bin/webpack.js $opt");# -d
     &$put_text("$build_dir/publish_time",time);
     &$put_text("$build_dir/c4gen.ht.links",join"",
