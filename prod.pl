2b7e1c596408bcf69c26de315b634c7715b9f9c8
Sergei Kirjanov <rewlad@gmail.com>
2022-03-18 13:15:19 +0200

    elector-debug

--- prod.pl
+++ prod.pl
@@ -4,7 +4,7 @@ use strict;
 use Digest::MD5 qw(md5_hex);
 use JSON::XS;
 
-my $sys_image_ver = "v86";
+my $sys_image_ver = "v91";
 
 sub so{ print join(" ",@_),"\n"; system @_; }
 sub sy{ print join(" ",@_),"\n"; system @_ and die $?; }
