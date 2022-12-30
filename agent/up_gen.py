9d7f84682fa1f08434b4aa785702890370aaf04f
Sergei Kirjanov <rewlad@gmail.com>
2022-12-30 02:31:01 +0200

    win-side-optim

--- agent/up_gen.py
+++ agent/up_gen.py
@@ -1,8 +1,14 @@
 
 import sys
+import pathlib
+
+def read_text(path_str):
+    return pathlib.Path(path_str).read_text(encoding='utf-8', errors='strict')
 
 def write_text(path, content):
     with open(path,"w") as f: f.write(content)
+
+def never(a): raise Exception(a)
 
 def gen_conf(context,path,comment,uid,home,setup,arch_name,repo_name,network):
     full_context = f"{home}/{repo_name}/{context}"
@@ -40,12 +46,38 @@ def gen_conf(context,path,comment,uid,home,setup,arch_name,repo_name,network):
         "ENV PATH=${PATH}:/c4/bin:/tools:/tools/sbt/bin",
     )))
 
+def gen_sync(context, repo_name):
+    replink = f"c4dep.ci.replink"
+    rels = ("", *(
+        local
+        for row in read_text(replink).splitlines()
+        for cmd, local_str, rem, comm in (row.split(),)
+        for local, dummy in (local_str.split("/"),)
+        if cmd == "C4REL" and dummy == "dummy" or never("bad rel")
+    ))
+    git_lines = [line for rel in rels for line in (
+        f"git -C ./{rel} rev-parse HEAD > target/c4sync-head-{rel}",
+        f"git -C ./{rel} status --porcelain=v1 --no-renames > target/c4sync-stat-{rel}",
+    )]
+    write_text(f"{context}/sync.bat", "\n".join((
+        f"docker exec -e C4REPO_MAIN_CONF=/c4repo/{repo_name}/{replink} c4agent_kc /replink.pl",
+        *git_lines,
+        "docker exec c4agent_kc sh -c '" +
+        "python3 -u $C4CI_PROTO_DIR/sync.py local" +
+        f" --from-dir /c4repo/{repo_name}" +
+        f" --to-dir /c4/{repo_name}" +
+        f" --rels {':'.join(rels)}" +
+        " && c4sync_remote" +
+        "'",
+    )))
+
 def main(repo_name):
     context = "agent"
     ports = "-p 127.0.0.1:1979:1979 -p 127.0.0.1:4005:4005 -e C4AGENT_IP=0.0.0.0"
     gen_conf(context,"up"    ,"","$(id -u)","$HOME/c4repo",["mkdir -p $HOME/c4repo"],"amd64",repo_name,"--network host -e C4AGENT_IP=127.0.0.1")
     gen_conf(context,"up-arm","","$(id -u)","$HOME/c4repo",["mkdir -p $HOME/c4repo"],"arm64",repo_name,ports)
     gen_conf(context,"up.bat","REM ","1000","%c4repo-path%",["set c4repo-path=c:/c4repo","REM change c4repo-path to your own"],"amd64",repo_name,ports)
+    gen_sync(context, repo_name)
 
 script, *args = sys.argv
 main(*args)
--- sync.py
+++ sync.py
@@ -1,45 +1,18 @@
 from c4util import parse_table, read_text, changing_text, changing_text_observe
-from build_util import never, need_dir, run, setup_parser
-
-
-def get_sync_rels(replink):
-    return ("", *(
-        local
-        for cmd, local_str, rem, comm in parse_table(read_text(replink))
-        for local, dummy in (local_str.split("/"),)
-        if cmd == "C4REL" and dummy == "dummy" or never("bad rel")
-    )), f"target/c4sync-head-", f"target/c4sync-stat-"
-
-
-def handle_gen(opt):
-    rels, head_path, stat_path = get_sync_rels(opt.replink)
-    git_lines = [line for rel in rels for line in (
-        f"git -C ./{rel} rev-parse HEAD > {head_path}{rel}",
-        f"git -C ./{rel} status --porcelain=v1 --no-renames > {stat_path}{rel}",
-    )]
-    need_dir(f"{opt.from_dir}/target")
-    changing_text(f"{opt.from_dir}/target/c4sync.bat", "\n".join((
-        f"docker exec -e C4REPO_MAIN_CONF={opt.replink} c4agent_kc /replink.pl",
-        *git_lines,
-        "docker exec c4agent_kc sh -c '" +
-        "python3 -u $C4CI_PROTO_DIR/sync.py local" +
-        f" --from-dir {opt.from_dir}" +
-        f" --to-dir {opt.to_dir}" +
-        f" --replink {opt.replink}" +
-        " && c4sync_remote" +
-        "'",
-    )))
+from build_util import need_dir, run, setup_parser
 
 
 def handle_local(opt):
-    rels, head_path, stat_path = get_sync_rels(opt.replink)
+    rels = opt.rels.split(":")
     will_commits = " ".join(
-        read_text(f"{opt.from_dir}/{head_path}{rel}").strip() for rel in rels
+        read_text(f"{opt.from_dir}/target/c4sync-head-{rel}").strip()
+        for rel in rels
     )
     add_files = {
         (f"{rel}/{fn}" if rel else fn)
         for rel in rels
-        for tp, fn in parse_table(read_text(f"{opt.from_dir}/{stat_path}{rel}"))
+        for tp, fn
+        in parse_table(read_text(f"{opt.from_dir}/target/c4sync-stat-{rel}"))
     }
     to_target_dir = f"{opt.to_dir}/target"
     commits_path = f"{to_target_dir}/c4sync-commits"
@@ -63,11 +36,9 @@ def handle_local(opt):
 
 def main():
     opt = setup_parser((
-        ("gen", handle_gen, ("--from-dir", "--to-dir", "--replink")),
-        ("local", handle_local, ("--from-dir", "--to-dir", "--replink")),
+        ("local", handle_local, ("--from-dir", "--to-dir", "--rels")),
     )).parse_args()
     opt.op(opt)
 
 
 main()
-
