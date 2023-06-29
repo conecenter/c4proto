6ff5e17db89094a94f5c492063c0225d44d8dcb9
Sergei Kirjanov <rewlad@gmail.com>
2023-06-29 15:02:53 +0300

    draft

--- ci_up.py
+++ ci_up.py
@@ -12,7 +12,8 @@ chart = {"apiVersion": "v2", "name": f"{release}.{state}", "version": "0"}
 Path(f"{chart_dir.name}/templates").mkdir()
 Path(f"{chart_dir.name}/Chart.yaml").write_text(dumps(chart, sort_keys=True), encoding="utf-8", errors="strict")
 Path(f"{chart_dir.name}/templates/identity.yaml").write_bytes(b"{{range .Values.manifests}}\n---\n{{toYaml .}}{{end}}")
-cmd = ("helm", "upgrade", "--install", "--wait", "--atomic", "--kube-context", context, release, chart_dir.name, "-f-")
+flags = ("--install", "--wait", "--atomic", "--timeout", "1000", "--kube-context", context)
+cmd = ("helm", "upgrade", *flags, release, chart_dir.name, "-f-")
 # --atomic to avoid manual rollbacks
 print("running: " + " ".join(cmd), file=sys.stderr)
 subprocess.run(cmd, check=True, text=True, input=dumps(info, sort_keys=True, indent=4))
