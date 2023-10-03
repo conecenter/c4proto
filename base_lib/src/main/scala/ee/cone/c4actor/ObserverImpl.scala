a37037a6279ae0cd0035fc2e3e1a15bab6f80d25
Sergei Kirjanov <rewlad@gmail.com>
2023-10-03 21:19:32 +0300

    draft

--- base_lib/src/main/scala/ee/cone/c4actor/ObserverImpl.scala
+++ base_lib/src/main/scala/ee/cone/c4actor/ObserverImpl.scala
@@ -2,13 +2,19 @@ package ee.cone.c4actor
 
 import java.time.Instant
 import com.typesafe.scalalogging.LazyLogging
+import ee.cone.c4actor.QProtocol.S_Firstborn
+import ee.cone.c4actor.TxStatusProtocol.{E_TxStatus, S_TxStatuses}
 import ee.cone.c4actor.Types.{SrcId, TransientMap}
-import ee.cone.c4di.{c4, provide}
+import ee.cone.c4assemble.Types.{Each, Values}
+import ee.cone.c4assemble.c4assemble
+import ee.cone.c4di.{c4, c4multi, provide}
+import ee.cone.c4proto.{Id, protocol}
 
 import java.util.concurrent.LinkedBlockingQueue
 import scala.annotation.tailrec
 import scala.collection.immutable.{Map, Seq}
-import scala.concurrent.Future
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future, Promise}
 
 trait TxTransforms {
   def get(global: RichContext): Map[SrcId,TransientMap=>TransientMap]
@@ -119,31 +125,87 @@ class SerialObserver(localStates: Map[SrcId,TransientMap])(
   private sealed trait ObservedEvent
   private final class TodoEv(val value: Map[SrcId, TransientMap => TransientMap]) extends ObservedEvent
   private final class DoneEv(val key: SrcId, val value: TransientMap) extends ObservedEvent
+  private final class ReportEv(val promise: Promise[Map[SrcId,Long]] = Promise()) extends ObservedEvent
   private val queue = new LinkedBlockingQueue[ObservedEvent]
-  def send(actions: Map[SrcId,TransientMap=>TransientMap]): Unit =
-    queue.put(new TodoEv(actions))
+  def send(actions: Map[SrcId,TransientMap=>TransientMap]): Unit = queue.put(new TodoEv(actions))
+  def report(): Future[Map[SrcId,Long]] = {
+    val ev = new ReportEv
+    queue.put(ev)
+    ev.promise.future
+  }
   def run(): Unit = iteration(Map.empty)
   private class ActorState(
-    val transient: TransientMap, val todo: Option[TransientMap=>TransientMap], val inProgress: Boolean
+    val transient: TransientMap, val todo: Option[TransientMap=>TransientMap],
+    val inProgress: Boolean, val startedAt: Long
   )
   private def checkActivate(k: SrcId, st: ActorState): ActorState = if(st.inProgress || st.todo.isEmpty) st else {
     execution.fatal(Future(queue.put(new DoneEv(k, st.todo.get(st.transient))))(_))
-    new ActorState(st.transient, None, true)
+    new ActorState(st.transient, None, true, System.currentTimeMillis)
   }
   @tailrec private def iteration(wasStates: Map[SrcId,ActorState]): Unit = iteration(queue.take() match {
     case ev: TodoEv =>
       (ev.value.keySet ++ wasStates.keySet).map{ k =>
-        val wasState = wasStates.getOrElse(k,new ActorState(Map.empty, None, false))
+        val was = wasStates.getOrElse(k,new ActorState(Map.empty, None, false, 0L))
         val action = ev.value(k)
-        k -> checkActivate(k, new ActorState(wasState.transient, Option(action), wasState.inProgress))
+        k -> checkActivate(k, new ActorState(was.transient, Option(action), was.inProgress, was.startedAt))
       }.toMap
     case ev: DoneEv =>
-      val state = checkActivate(ev.key, new ActorState(ev.value, wasStates(ev.key).todo, false))
+      val state = checkActivate(ev.key, new ActorState(ev.value, wasStates(ev.key).todo, false, 0L))
       if(state.transient.isEmpty && state.todo.isEmpty && !state.inProgress)
         wasStates - ev.key  else wasStates + (ev.key->state)
+    case ev: ReportEv =>
+      val res = wasStates.collect{ case (k,v) if v.inProgress => (k,v.startedAt) }
+      execution.success(ev.promise, res)
+      wasStates
   })
 }
 
+@protocol("ParallelObserversApp") object TxStatusProtocol {
+  case class S_TxStatuses(
+    @Id(0x00B1) electorClientId: SrcId,
+    statuses: List[E_TxStatus]
+  )
+  case class E_TxStatus(
+    @Id(0x00B2) txTrId: SrcId,
+    @Id(0x0075) startedAt: Long,
+  )
+}
+
+@c4assemble("ParallelObserversApp") class TxStatusAssembleBase(factory: TxStatusTrackerTxFactory){
+  def join(
+    srcId: SrcId,
+    firstborn: Each[S_Firstborn]
+  ): Values[(SrcId, EnabledTxTr)] =
+    List(WithPK(EnabledTxTr(factory.create("TxStatusTrackerTx"))))
+}
+
+@c4multi("ParallelObserversApp") final case class TxStatusTrackerTx(srcId: SrcId)(
+  txAdd: LTxAdd,
+  ex: ParallelObserverExecutable,
+  currentProcess: CurrentProcess,
+  getS_TxStatuses: GetByPK[S_TxStatuses],
+) extends TxTransform with LazyLogging {
+  def transform(local: Context): Context = {
+    val was = getS_TxStatuses.ofA(local).get(currentProcess.id).toList
+    val res = Await.result(ex.report(System.currentTimeMillis-5000), Duration.Inf)
+    val will = res.toList.sortBy(_._1).map{ case (k,v) => E_TxStatus(k,v) } match {
+      case Seq() => Nil
+      case l => List(S_TxStatuses(currentProcess.id, l))
+    }
+    if(was == will) SleepUntilKey.set(Instant.now.plusSeconds(1))(local) else {
+      val events = if(will.isEmpty) was.flatMap(LEvent.delete) else will.flatMap(LEvent.update)
+      logger.warn(events.toString)
+      txAdd.add(events)(local)
+    }
+  }
+}
+
+// global purger
+//trait TxStatus {
+//  def report(): Future[Seq[(SrcId,Long)]]
+//}
+//for((k,v) <- wasStates if v.startedAt + 15000 < System.currentTimeMillis) logger.warn(s"")
+//Await.result(ev.promise.future, Duration.Inf)
 
 
 
