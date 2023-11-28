a3240c4ebbf0ba4a5f3951eaa6f207eed1156991
Aleafan <76619902+Aleafan@users.noreply.github.com>
2023-11-28 10:05:54 +0100

    Aaf/node update (#395)
    * update node to v.20.5.0
    
    * fix build_client syntax
    
    * change node version to 16.20.2
    
    * update node to v20.9.0
    
    * add --legacy-peer-deps flag for npm to silence peer deps errors
    
    ---------
    
    Co-authored-by: Aleafan <aaf@cone.ee>

--- prod.pl
+++ prod.pl
@@ -456,13 +456,13 @@ my $if_changed = sub{
 };
 my $build_client = sub{
     my($dir, $mode)=@_;
-    my $opt = $mode eq "fast" ? "--env.fast=true --mode development" : $mode eq "dev" ? "--mode development" :
+    my $opt = $mode eq "fast" ? "--env fast=true --mode development" : $mode eq "dev" ? "--mode development" :
         "--mode production";
     my $build_dir = "$dir/out";
     unlink or die $! for <$build_dir/*>;
     my $conf_dir = &$single_or_undef(grep{-e} map{"$_/webpack"} <$dir/src/*>) || die;
     &$if_changed("$dir/package.json", &$get_text("$conf_dir/package.json"), sub{1})
-        and sy("cd $dir && npm install --no-save"); # --legacy-peer-deps
+        and sy("cd $dir && npm install --no-save --legacy-peer-deps");
     sy("cd $dir && cp $conf_dir/webpack.config.js . && cp $conf_dir/tsconfig.json . && node_modules/webpack/bin/webpack.js --color $opt");# -d
     &$put_text("$build_dir/publish_time",time);
     &$put_text("$build_dir/c4gen.ht.links",join"",
