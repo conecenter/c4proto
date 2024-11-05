2f92d45920f8534ca34962b2d759eec196ecd7d3
Sergei Kirjanov <sk@cone.ee>
2024-11-05 13:21:54 +0200

    draft

--- base_lib/src/main/scala/ee/cone/c4ui/UITxImpl.scala
+++ base_lib/src/main/scala/ee/cone/c4ui/UITxImpl.scala
@@ -35,8 +35,13 @@ import scala.Function.chain
     key: SrcId,
     tasks: Values[FromAlienTask],
     @by[BranchKey] requests: Values[BranchMessage],
-  ): Values[(SrcId,TxTransform)] =
-    List(WithPK(txFactory.create(key, tasks.map(_.fromAlienState.sessionKey).sorted, requests.sortBy(ToPrimaryKey(_)))))
+  ): Values[(SrcId,TxTransform)] = tasks match {
+    case Seq(task) =>
+      def long(v: String) = if(v.isEmpty) 0L else try v.toLong catch { case e: NumberFormatException => 0L }
+      val req = requests.collect{case m: VDomMessage => m}.minByOption(r=>(long(r.header("x-r-index")),ToPrimaryKey(r)))
+      List(WithPK(txFactory.create(key, task.fromAlienState.sessionKey, req)))
+    case _ => Nil
+  }
 }
 
 @c4multi("AlienExchangeCompApp") final case class FromAlienTaskImpl(
@@ -55,24 +60,14 @@ import scala.Function.chain
 
 @c4("UICompApp") final class EnableBranchScaling extends EnableSimpleScaling(classOf[UITx])
 
-@c4multi("UICompApp") final case class UITx(
-  branchKey: String, sessionKeys: List[String], requests: List[BranchMessage with VDomMessage]
+@c4multi("UICompApp") final case class UIPostHandler(
+  branchKey: String, sessionKey: String, request: BranchMessage with VDomMessage
 )(
-  getView: GetByPK[View],
-  txAdd: LTxAdd, catchNonFatal: CatchNonFatal, listConfig: ListConfig,
-  branchErrorSaver: Option[BranchErrorSaver], branchOperations: BranchOperations,
-  eventLogUtil: EventLogUtil, sessionUtil: SessionUtil,
-  vDomResolver: VDomResolver, vDomHandler: VDomHandler, vDomUntil: VDomUntil, child: ChildPairFactory,
-  rootTags: RootTags, setLocationReceiverFactory: SetLocationReceiverFactory,
-) extends TxTransform with LazyLogging {
+  listConfig: ListConfig, branchErrorSaver: Option[BranchErrorSaver], catchNonFatal: CatchNonFatal,
+  sessionUtil: SessionUtil, vDomResolver: VDomResolver,
+) extends LazyLogging {
+  // do
   private val sessionTimeoutSec = Single.option(listConfig.get("C4STATE_REFRESH_SECONDS")).fold(100L)(_.toLong)
-  private def ack(local: Context, request: BranchMessage): LEvents = {
-    val (k,v) = (request.header("x-r-reload"), request.header("x-r-index"))
-    if(k.nonEmpty && v.nonEmpty) sessionUtil.addAck(local, Single(sessionKeys), k, v) else Nil
-  }
-  private def eventLogChanges(local: Context, res: PostViewResult): LEvents =
-    if(res.diff.isEmpty && res.snapshot.isEmpty) Nil
-    else eventLogUtil.write(local, branchKey, res.diff, Option(res.snapshot).filter(_.nonEmpty))
   private def handleError(local: Context, err: Throwable) =
     (local, branchErrorSaver.toSeq.flatMap(_.saveErrors(local, branchKey, err)))
   private def measure[T](before: String)(f: ()=>T)(after: Long=>String): T = {
@@ -83,12 +78,12 @@ import scala.Function.chain
     res
   }
   //dispatches incoming message // can close / set refresh time
-  private def dispatch(local: Context, request: VDomMessage with BranchMessage): (Context, LEvents) = {
+  private def dispatch(local: Context): (Context, LEvents) = catchNonFatal{
     assert(request.method == "POST")
     request.header("x-r-op") match {
       case "redraw" => (resetUntil(local), Nil)
       case "online" =>
-        (local, sessionUtil.setStatus(local, Single(sessionKeys), sessionTimeoutSec, request.body.size > 0))
+        (local, sessionUtil.setStatus(local, sessionKey, sessionTimeoutSec, request.body.size > 0))
       case "" =>
         val path = request.header("x-r-vdom-path")
         if(path.nonEmpty) vDomResolver.resolve(path)(VDomStateKey.of(local).map(_.value)) match {
@@ -99,25 +94,33 @@ import scala.Function.chain
             (local, Nil)
         } else (local, Nil)
     }
-  }
+  }(s"branch $branchKey dispatch failed")(handleError(local, _))
   private def resetUntil: Context => Context = VDomStateKey.modify(_.map(st=>st.copy(until = 0)))
-  private def handle(local: Context, request: BranchMessage with VDomMessage): (Context, LEvents) =
-    measure(s"branch $branchKey tx begin ${request.header("x-r-alien-date")}"){() =>
-
-      val (nLocal, lEvents) =
-        catchNonFatal{ dispatch(local, request) }(s"branch $branchKey tx failed")(handleError(local, _))
-      (nLocal, lEvents ++ ack(local, request))
-      
-    }(t => s"branch $branchKey tx done in $t ms")
-  private def handleView(local: Context): (Context, LEvents) =
-    vDomHandler.preView(VDomStateKey.of(local)).fold((local,Nil:LEvents)){ preViewRes =>
-      handleReView(chain(Seq(
-        VDomStateKey.set(Option(preViewRes.clean)),
-        CurrentBranchKey.set(branchKey),
-      ))(local), preViewRes)
+  private def dedupAck(local: Context): (Context, LEvents) = catchNonFatal{
+    (request.header("x-r-reload"), request.header("x-r-index")) match {
+      case ("", "") => dispatch(local)
+      case (k, vStr) if k.nonEmpty && vStr.nonEmpty =>
+        if(sessionUtil.ackList(local, sessionKey).exists(h => h.key == k && vStr.toLong <= h.value.toLong)) (local, Nil)
+        else dispatch(local) match {
+          case (nLocal, lEvents) => (nLocal, lEvents ++ sessionUtil.addAck(local, sessionKey, k, vStr))
+        }
     }
+  }(s"branch $branchKey dedupAck failed")(handleError(local, _))
+  def handle(local: Context): (Context, LEvents) =
+    measure(s"branch $branchKey tx begin ${request.header("x-r-alien-date")}"){() =>
+      dedupAck(local)
+    }(t => s"branch $branchKey tx done in $t ms")
+}
+
+@c4multi("UICompApp") final case class UIViewer(branchKey: String, sessionKey: String)(
+  catchNonFatal: CatchNonFatal, sessionUtil: SessionUtil, eventLogUtil: EventLogUtil,
+  branchOperations: BranchOperations, getView: GetByPK[View], vDomHandler: VDomHandler, vDomUntil: VDomUntil,
+  rootTags: RootTags, setLocationReceiverFactory: SetLocationReceiverFactory,
+){
+  private def eventLogChanges(local: Context, res: PostViewResult): LEvents =
+    if(res.diff.isEmpty && res.snapshot.isEmpty) Nil
+    else eventLogUtil.write(local, branchKey, res.diff, Option(res.snapshot).filter(_.nonEmpty))
   private def handleReView(local: Context, preViewRes: PreViewResult): (Context, LEvents) = {
-    val sessionKey = Single(sessionKeys)
     val location = sessionUtil.location(local, sessionKey)
     val statusEl = rootTags.statusElement("status", location, setLocationReceiverFactory.create(sessionKey))
     val ackList = sessionUtil.ackList(local, sessionKey)
@@ -135,16 +138,30 @@ import scala.Function.chain
     val seedChanges = branchOperations.saveChanges(local, branchKey, seeds)
     (VDomStateKey.set(Option(nState))(local), seedChanges ++ eventLogChanges(local, res))
   }
+  def handle(local: Context): (Context, LEvents) =
+    vDomHandler.preView(VDomStateKey.of(local)).fold((local,Nil:LEvents)){ preViewRes =>
+      handleReView(chain(Seq(
+        VDomStateKey.set(Option(preViewRes.clean)),
+        CurrentBranchKey.set(branchKey),
+      ))(local), preViewRes)
+    }
+}
+
+@c4multi("UICompApp") final case class UITx(
+  branchKey: String, sessionKey: String, reqOpt: Option[BranchMessage with VDomMessage]
+)(
+  txAdd: LTxAdd, sessionUtil: SessionUtil, branchOperations: BranchOperations,
+  uiViewerFactory: UIViewerFactory, uiPostHandlerFactory: UIPostHandlerFactory,
+) extends TxTransform with LazyLogging {
   private def purge(local: Context) =
-    (local, sessionUtil.purge(local, Single(sessionKeys)) ++ branchOperations.purge(local, branchKey))
+    (local, sessionUtil.purge(local, sessionKey) ++ branchOperations.purge(local, branchKey))
   def transform(local: Context): Context = {
-    val requestOpt = requests.collect{ case m: VDomMessage => m }
-      .minByOption(_.header("x-r-index") match { case "" => 0L case s => s.toLong })
     val (nLocal, lEvents): (Context, LEvents) =
-      if(sessionKeys.isEmpty) (local, Nil:LEvents)
-      else if(sessionUtil.expired(local, Single(sessionKeys))) purge(local)
-      else requestOpt.fold(handleView(local))(handle(local, _))
-    txAdd.add(lEvents ++ requestOpt.toSeq.flatMap(_.deletes))(nLocal)
+      if(sessionUtil.expired(local, sessionKey)) purge(local)
+      else reqOpt.fold(uiViewerFactory.create(branchKey, sessionKey).handle(local))(
+        uiPostHandlerFactory.create(branchKey, sessionKey, _).handle(local)
+      )
+    txAdd.add(lEvents)(nLocal)
   }
 }
 
