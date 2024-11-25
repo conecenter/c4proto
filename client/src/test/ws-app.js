6def161224608d08e51089112149cbdd7d551dcd
Sergei Kirjanov <sk@cone.ee>
2024-11-25 15:23:30 +0200

    fix

--- client/src/test/ws-app.js
+++ client/src/test/ws-app.js
@@ -1,12 +1,12 @@
 
 import {CanvasUtil,ExchangeCanvasSetup,CanvasFactory} from "../main/canvas"
-import {CanvasBaseMix} from "../main/canvas-mix"
+import {CanvasBaseMix,CanvasSimpleMix} from "../main/canvas-mix"
 import {main} from "./ws-app.tsx"
 
 const log = v => console.log(v)
 const util = CanvasUtil()
 const exchangeMix = options => canvas => ExchangeCanvasSetup(canvas)
-const canvasMods = [CanvasBaseMix(log,util),exchangeMix]
+const canvasMods = [CanvasBaseMix(log,util),CanvasSimpleMix(),exchangeMix]
 const canvasFactory = CanvasFactory(util, canvasMods)
 
 main({win: window, canvasFactory})
