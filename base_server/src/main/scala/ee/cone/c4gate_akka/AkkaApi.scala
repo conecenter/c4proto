7c3048f75d9d95cc9732f30dcd5d2f2a1ae8713c
Sergei Kirjanov <rewlad@gmail.com>
2025-02-06 15:50:29 +0200

    akka-cleanup-draft

--- base_server/src/main/scala/ee/cone/c4gate_akka/AkkaApi.scala
+++ base_server/src/main/scala/ee/cone/c4gate_akka/AkkaApi.scala
@@ -1,24 +1,12 @@
 package ee.cone.c4gate_akka
 
-import akka.stream.ActorMaterializer
-
 import scala.concurrent.{ExecutionContext, Future}
+import akka.stream.ActorMaterializer
 import akka.http.scaladsl.model.{HttpRequest,HttpResponse}
-import akka.http.scaladsl.HttpExt
 
 trait AkkaMat {
   def get: Future[ActorMaterializer]
 }
-
-abstract class AkkaConf {
-  def content: String
-}
-
-trait AkkaHttp {
-  def get: Future[HttpExt]
-}
-
-/******************************************************************************/
 
 trait AkkaRequestHandler {
   def pathPrefix: String
--- base_server/src/main/scala/ee/cone/c4gate_akka/AkkaMix.scala
+++ base_server/src/main/scala/ee/cone/c4gate_akka/AkkaMix.scala
@@ -1,13 +1,6 @@
 
 package ee.cone.c4gate_akka
 
-//import ee.cone.c4actor_repl_impl.SSHDebugApp
-import ee.cone.c4di.c4app
 import ee.cone.c4gate_server.AbstractHttpGatewayApp
-import ee.cone.c4gate.RoomsConfProtocolApp
 
-trait AkkaMatAppBase
-trait AkkaGatewayAppBase extends AbstractHttpGatewayApp with AkkaMatApp with RoomsConfProtocolApp
-
-//@c4app class
-trait SimpleAkkaGatewayAppBase extends AkkaGatewayApp //with SSHDebugApp
+trait AkkaGatewayAppBase extends AbstractHttpGatewayApp
