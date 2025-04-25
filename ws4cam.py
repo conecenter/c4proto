1abc0830b36656077ca07ac875468f530ca838cb
Ilya Kachalsky <ilya.kachalsky@gmail.com>
2025-04-25 14:35:02 +0300

    cam fix (#411)

--- ws4cam.py
+++ ws4cam.py
@@ -1,3 +1,4 @@
+import json
 
 import av
 import faulthandler
@@ -13,15 +14,20 @@ from re import fullmatch
 
 def log(text): print(text, file=stderr)
 
+def get_cred(cam_id):
+    return cred_map.get(cam_id, cred_map.get("default", ""))
+
 def handler(ws):
     req_id = next(counter)
-    url = f'http://{environ["C4CAM_RESOLVER"]}/addr4key/{fullmatch(r"/mjpeg/([\w\-]+)", ws.request.path).group(1)}'
+    cam_id = fullmatch(r"/mjpeg/([\w\-]+)", ws.request.path).group(1)
+    url = f'http://{environ["C4CAM_RESOLVER"]}/addr4key/{cam_id}'
     log(f"{req_id} starting {url}")
     resp = urlopen(url)
     if resp.status != 200: raise Exception("bad response")
     cam_addr = resp.read().decode("utf-8")
     log(f"{req_id} resolved {cam_addr}")
     connected_at, last_received_at = (monotonic(), monotonic())
+    cred = get_cred(cam_id)
     try:
         with av.open(f"rtsp://{cred}@{cam_addr}",options={"rtsp_transport": "tcp"}) as container:
             log(f"{req_id} ready")
@@ -44,5 +50,5 @@ def handler(ws):
 faulthandler.enable()
 #faulthandler.dump_traceback_later(30, repeat=True)
 counter = count()
-cred = Path(environ["C4CAM_AUTH"]).read_text(encoding="utf-8", errors="strict")
+cred_map = json.loads(Path(environ["C4CAM_AUTH"]).read_text(encoding="utf-8", errors="strict"))
 serve(handler, "0.0.0.0", int(environ["C4CAM_SERVER_PORT"])).serve_forever()
