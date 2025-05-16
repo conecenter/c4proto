bf26a950e5f1fd0e60e7aa6e7410df84409b96e9
Sergei Kirjanov <rewlad@gmail.com>
2025-05-16 19:05:13 +0300

    -

--- kui/app.jsx
+++ kui/app.jsx
@@ -2,7 +2,7 @@
 import React from "react"
 import {createRoot} from "react-dom/client"
 
-const exchHello = ev => fetch("/kop",{method:"POST",body:{op:"get_state"}}).then(resp=>alert(resp))
+const exchHello = ev => fetch("/kop",{method:"POST",body:JSON.stringify({op:"get_state"})}).then(resp=>alert(resp))
 
 const App = () => <h1 onClick={exchHello}>Hello, world!</h1>
 
