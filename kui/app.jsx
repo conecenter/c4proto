d51a3b37f358c9e3193b6602cbe174793398f069
Sergei Kirjanov <rewlad@gmail.com>
2025-05-16 18:55:22 +0300

    -

--- kui/app.jsx
+++ kui/app.jsx
@@ -4,7 +4,7 @@ import {createRoot} from "react-dom/client"
 
 const exchHello = ev => fetch("/kop",{method:"POST",body:{op:"get_state"}}).then(resp=>alert(resp))
 
-const App = () => <h1 onclick={exchHello}>Hello, world!</h1>
+const App = () => <h1 onClick={exchHello}>Hello, world!</h1>
 
 const main= () => {
     const rootNativeElement = document.createElement("span")
