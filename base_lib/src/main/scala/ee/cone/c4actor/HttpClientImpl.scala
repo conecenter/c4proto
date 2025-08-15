9a564ab612753d6ba942666f8f980798aa790ab5
Sergei Kirjanov <rewlad@gmail.com>
2025-08-15 14:54:17 +0300

    s3fix

--- base_lib/src/main/scala/ee/cone/c4actor/HttpClientImpl.scala
+++ base_lib/src/main/scala/ee/cone/c4actor/HttpClientImpl.scala
@@ -3,11 +3,16 @@ package ee.cone.c4actor
 import ee.cone.c4di.c4
 
 import java.net.http.HttpClient
+import java.net.http.HttpClient.Version.HTTP_1_1
 import scala.concurrent.{Future, Promise}
 
-@c4("HttpClientApp") final class HttpClientProviderImpl(execution: Execution)(
+@c4("HttpClientApp") final class HttpClientProviderImpl(execution: Execution, config: ListConfig)(
   clientPromise: Promise[HttpClient] = Promise(),
 ) extends HttpClientProvider with Executable with Early {
-  def run(): Unit = execution.success(clientPromise, HttpClient.newHttpClient)
+  def run(): Unit = {
+    val downgrade = config.get("C4HTTP_DOWNGRADE").exists(_.nonEmpty)
+    val client = if(downgrade) HttpClient.newBuilder().version(HTTP_1_1).build() else HttpClient.newHttpClient
+    execution.success(clientPromise, client)
+  }
   def get: Future[HttpClient] = clientPromise.future
 }
