595ac51c4fed88bdeadfaa8b1bd5d1caf5bb9cc2
Sergei Kirjanov <rewlad@gmail.com>
2026-03-24 16:40:14 +0200

    kafka-ver

--- prod.pl
+++ prod.pl
@@ -3,7 +3,7 @@
 use strict;
 use Digest::MD5 qw(md5_hex);
 
-my $sys_image_ver = "v70";
+my $sys_image_ver = "v70a";
 
 sub so{ print join(" ",@_),"\n"; system @_; }
 sub sy{ print join(" ",@_),"\n"; system @_ and die $?; }
