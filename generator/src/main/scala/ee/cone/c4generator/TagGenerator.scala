f43c29668661ab06d4393126724f05f989307edb
Aleafan <aaf@cone.ee>
2026-06-09 12:33:06 +0200

    enhance TsTagWillGenerator to map "Em" type to "number" in ts generation

--- generator/src/main/scala/ee/cone/c4generator/TagGenerator.scala
+++ generator/src/main/scala/ee/cone/c4generator/TagGenerator.scala
@@ -424,7 +424,7 @@ class TsTagWillGenerator extends WillGenerator {
     case "Boolean"                 => "boolean"
     case "Int" | "Double" | "Float" => "number"
     case "Long"                    => "string" // Long can't round-trip through JS number; sent as string
-    case "BigDecimal"              => "number"
+    case "Em" | "BigDecimal"       => "number"
     case n if switchTraitNames(n)  => n
     case n => commonImports += n; n
   }
